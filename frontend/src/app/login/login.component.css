3b849c16877e24d75d1839b56598e35dcf48553a
Jason White <jmw5598@gmail.com>
2017-02-14 14:47:30 -0500

    created login component

---
+++ frontend/src/app/login/login.component.css
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'gro-login',
+	templateUrl: './login.component.html';
+	styleUrls: ['./login.component.css'];
+})
+export class LoginComponent {}
