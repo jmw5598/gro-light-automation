f463a67ade3815653b8cf503685e972ebf888694
Jason White <jmw5598@gmail.com>
2017-02-19 15:38:32 -0500

    added GeneratedValue annotation to id attribute of Pin

--- backend/src/main/java/com/gro/model/Pin.java
+++ backend/src/main/java/com/gro/model/Pin.java
@@ -2,6 +2,8 @@ package com.gro.model;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -15,6 +17,7 @@ import com.gro.model.sensor.Sensor;
 public class Pin {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     @NotNull
