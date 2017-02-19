1e9adb9db28a4a3bc899e0b38e133b3ff27cb3e0
Jason White <jmw5598@gmail.com>
2017-02-19 18:16:44 -0500

    updated SensorController

--- backend/src/main/java/com/gro/controller/SensorController.java
+++ backend/src/main/java/com/gro/controller/SensorController.java
@@ -1,5 +1,33 @@
 package com.gro.controller;
 
-public class SensorController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gro.model.sensor.Sensor;
+import com.gro.repository.SensorRepository;
+
+
+@RestController
+@RequestMapping("/sensor")
+public class SensorController {
+    
+    @Autowired
+    private SensorRepository sensorRepository;
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public void create(@RequestBody Sensor sensor) {
+        System.out.println("sensor: " + sensor);
+        sensorRepository.save(sensor);
+    }
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public List<Sensor> getAll() {
+        return sensorRepository.findAll();
+    }
+    
 }
