406cad869aa1dd711c1c022905a9e7b98b73e00c
Jason White <jmw5598@gmail.com>
2017-02-19 18:15:42 -0500

    added @JsonIgnore on Sensor mapping to prevent infinite recusion

--- backend/src/main/java/com/gro/model/Pin.java
+++ backend/src/main/java/com/gro/model/Pin.java
@@ -1,14 +1,14 @@
 package com.gro.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gro.model.sensor.Sensor;
 
 
@@ -22,10 +22,15 @@ public class Pin {
     @NotNull
     private String alias;
 
-    @OneToOne(mappedBy = "pin", fetch = FetchType.EAGER)
+    @OneToOne(mappedBy = "pin", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonIgnore
     private Sensor sensor;
 
     public Pin() { }
+    
+    public Pin(String alias) {
+        this.alias = alias;
+    }
 
     public Pin(int id, String alias) {
         this.id = id;
