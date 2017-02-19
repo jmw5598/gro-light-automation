5d332e205467033fc5ead56962d0667e1ad6b835
Jason White <jmw5598@gmail.com>
2017-02-19 16:24:52 -0500

    created PinController

---
+++ backend/src/main/java/com/gro/controller/PinController.java
@@ -0,0 +1,39 @@
+package com.gro.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gro.model.Pin;
+import com.gro.repository.PinRepository;
+
+@RestController
+@RequestMapping("/pin")
+public class PinController {
+    
+    @Autowired
+    private PinRepository pinRepository;
+    
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public List<Pin> getAll() {
+        return pinRepository.findAll();
+    }
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public void savePin(@RequestBody Pin pin) {
+        System.out.println(pin.getAlias());
+        pinRepository.save(pin);
+    }
+    
+    @RequestMapping(path="/{id}", method=RequestMethod.GET)
+    public Pin getPin(@PathVariable("id") int id) {
+        return pinRepository.findOne(id);
+    }
+    
+}
