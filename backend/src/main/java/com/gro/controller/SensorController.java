99d89fdcf944830bf454923b06927b72d6726045
Jason White <jmw5598@gmail.com>
2017-02-20 16:01:41 -0500

    added endpoints to get/save sensor data

--- backend/src/main/java/com/gro/controller/SensorController.java
+++ backend/src/main/java/com/gro/controller/SensorController.java
@@ -3,12 +3,15 @@ package com.gro.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gro.model.sensor.Sensor;
+import com.gro.model.sensor.SensorData;
+import com.gro.repository.SensorDataRepository;
 import com.gro.repository.SensorRepository;
 
 
@@ -19,6 +22,10 @@ public class SensorController {
     @Autowired
     private SensorRepository sensorRepository;
     
+    @Autowired
+    private SensorDataRepository sensorDataRepository;
+    
+    
     @RequestMapping(method=RequestMethod.POST)
     public void create(@RequestBody Sensor sensor) {
         System.out.println("sensor: " + sensor);
@@ -28,6 +35,24 @@ public class SensorController {
     @RequestMapping(method=RequestMethod.GET)
     public List<Sensor> getAll() {
         return sensorRepository.findAll();
+    }
+    
+    @RequestMapping(path="/{id}", method=RequestMethod.GET)
+    public Sensor getSensor(@PathVariable("id") int id) {
+        return sensorRepository.findOne(id);
+    }
+    
+    @RequestMapping(path="/{id}/data", method=RequestMethod.GET)
+    public List<SensorData> getAllSensorData(@PathVariable("id") int id) {
+        Sensor sensor = sensorRepository.findOne(id);
+        return sensorDataRepository.findAllBySensor(sensor);
+    }
+    
+    @RequestMapping(path="/{id}/data", method=RequestMethod.POST)
+    public void saveSensorData(@PathVariable("id") int id, @RequestBody SensorData data) {
+        Sensor sensor = sensorRepository.findOne(id);
+        data.setSensor(sensor);
+        sensorDataRepository.save(data);
     }
     
 }
