9657d74cd72e4df90c890a7f390403012260f7e9
Jason White <jmw5598@gmail.com>
2017-02-23 17:19:31 -0500

    extended SensorDataRepositoryCustom

--- backend/src/main/java/com/gro/repository/SensorDataRepository.java
+++ backend/src/main/java/com/gro/repository/SensorDataRepository.java
@@ -3,13 +3,18 @@ package com.gro.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import com.gro.model.Pin;
 import com.gro.model.sensor.Sensor;
 import com.gro.model.sensor.SensorData;
+import com.gro.repository.SensorDataRepositoryCustom;
 
-public interface SensorDataRepository extends CrudRepository<SensorData, Integer>{
+@Repository
+public interface SensorDataRepository extends CrudRepository<SensorData, Integer>, SensorDataRepositoryCustom {
+    
     @Override
     public List<SensorData> findAll();
+    
     public List<SensorData> findAllBySensor(Sensor sensor);
+    
 }
