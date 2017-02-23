b28c3e0e139af5069dee1ed8cdfbd491879d8bfb
Jason White <jmw5598@gmail.com>
2017-02-23 17:17:15 -0500

    added @IntegrationComponentScan

--- backend/src/main/java/com/gro/DemoApplication.java
+++ backend/src/main/java/com/gro/DemoApplication.java
@@ -2,8 +2,10 @@ package com.gro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @SpringBootApplication
+@IntegrationComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
