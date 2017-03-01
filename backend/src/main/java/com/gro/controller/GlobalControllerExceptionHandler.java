837e30c952dabc04661d4108aaba13c502274934
Jason White <jmw5598@gmail.com>
2017-03-01 14:51:27 -0500

    removed exception handling for PinNotFound after code refactory, not needed

--- backend/src/main/java/com/gro/controller/GlobalControllerExceptionHandler.java
+++ backend/src/main/java/com/gro/controller/GlobalControllerExceptionHandler.java
@@ -9,21 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
 import com.gro.model.ApiError;
-import com.gro.model.PinNotFoundException;
 import com.gro.model.sensor.SensorNotFoundException;
 
 @ControllerAdvice
 @RestController
 public class GlobalControllerExceptionHandler {
-    
-    @ExceptionHandler(PinNotFoundException.class)
-    public ResponseEntity<Object> handlePinNotFound(PinNotFoundException e, WebRequest request) {
-        String error = e.getMessage();
-        ApiError apiError = 
-                new ApiError(HttpStatus.NOT_FOUND, e.getLocalizedMessage(), error);
-        
-        return new ResponseEntity<Object>(apiError, new HttpHeaders(), apiError.getStatus());
-    }
     
     @ExceptionHandler(SensorNotFoundException.class)
     public ResponseEntity<Object> handleSensorNotFound(SensorNotFoundException e, WebRequest request) {
