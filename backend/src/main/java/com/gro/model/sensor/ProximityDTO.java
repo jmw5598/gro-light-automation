e499c9928d28c6d3c908008720de83fb53bc30fb
Jason White <jmw5598@gmail.com>
2017-03-01 16:23:12 -0500

    created proximity dto for messaging

---
+++ backend/src/main/java/com/gro/model/sensor/ProximityDTO.java
@@ -0,0 +1,26 @@
+package com.gro.model.sensor;
+
+public class ProximityDTO {
+    
+    private double proximity;
+    private int sensorId;
+    
+    public ProximityDTO() {}
+    
+    public double getProximity() {
+        return this.proximity;
+    }
+    
+    public void setProximity(double proximity) {
+        this.proximity = proximity;
+    }
+    
+    public int getSensorId() {
+        return this.sensorId;
+    }
+    
+    public void setSensorId(int sensorId) {
+        this.sensorId = sensorId;
+    }
+    
+}
