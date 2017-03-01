2406d975957fac72f1b62417acb5fc2305a62318
Jason White <jmw5598@gmail.com>
2017-03-01 15:27:58 -0500

    updated messages routing

--- backend/src/main/java/com/gro/messaging/mqtt/MqttMessageRouter.java
+++ backend/src/main/java/com/gro/messaging/mqtt/MqttMessageRouter.java
@@ -4,17 +4,29 @@ import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.Router;
 import org.springframework.messaging.handler.annotation.Header;
 
+import com.gro.model.sensor.SensorType;
+
 @MessageEndpoint
 public class MqttMessageRouter {
     
     @Router(inputChannel="mqttRouterChannel")
     public String route(@Header("mqtt_topic") String topic) {
         String route = null;
-        switch(topic) {
-            case ("/temp"):
-                route = "dht22MessageTransformerChannel";
+        SensorType type = SensorType.valueOf(topic);
+        
+        switch(type) {
+            case TEMPERATURE:
+                route = "temperatureTransformerChannel";
                 break;
-            // add more cases for other topics
+            case HUMIDITY:
+                route = "humidityTransformerChannel";
+                break;
+            case TEMPERATURE_HUMIDITY:
+                route = "temperatureHumidityTransformerChannel";
+                break;
+            case PROXIMITY:
+                route = "proximityTransformerChannel";
+                break;
         }
         return route;
     }
