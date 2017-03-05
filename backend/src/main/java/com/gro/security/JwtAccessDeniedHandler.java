f2b2ea649ab40617e2743e459c5b2b9dc5c8a4ff
Jason White <jmw5598@gmail.com>
2017-03-05 15:07:23 -0500

    modified to return json api error message for Unauthorized and Access Denied

--- backend/src/main/java/com/gro/security/JwtAccessDeniedHandler.java
+++ backend/src/main/java/com/gro/security/JwtAccessDeniedHandler.java
@@ -1,22 +1,45 @@
 package com.gro.security;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
+import com.gro.model.ApiError;
+
+
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
-
+    
+    @Autowired
+    public Jackson2JsonObjectMapper jackson2JsonObjectMapper;
+    
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
-            AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Unauthorized");        
+            AccessDeniedException e) throws IOException, ServletException {
+        
+        ApiError error = new ApiError(HttpStatus.FORBIDDEN, e.getLocalizedMessage(), e.getMessage());
+        
+        try {
+            String json = jackson2JsonObjectMapper.toJson(error);
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+            response.getWriter().write(json);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        
     }
 
 }
--- backend/src/main/java/com/gro/security/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/gro/security/JwtAuthenticationEntryPoint.java
@@ -1,25 +1,45 @@
 package com.gro.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import com.gro.model.ApiError;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
 
     private static final long serialVersionUID = -8970718410437077606L;
-
+    
+    @Autowired
+    private Jackson2JsonObjectMapper jackson2JsonObjectMapper;
+    
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
-                         AuthenticationException authException) throws IOException {
-        // This is invoked when user tries to access a secured REST resource without supplying any credentials
-        // We should just send a 401 Unauthorized response because there is no 'login page' to redirect to
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                         AuthenticationException e) throws IOException {
+        
+        ApiError error = new ApiError(HttpStatus.FORBIDDEN, e.getLocalizedMessage(), e.getMessage());
+        
+        try {
+            String json = jackson2JsonObjectMapper.toJson(error);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+            response.getWriter().write(json);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        
     }
 }
