fe661f820ee6de3f89c7c551960494e0fdfad635
Jason White <jmw5598@gmail.com>
2017-03-05 13:33:54 -0500

    enabled method level security

--- backend/src/main/java/com/gro/security/SecurityConfig.java
+++ backend/src/main/java/com/gro/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,6 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Autowired
@@ -47,7 +49,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         
         http
             .csrf().disable()
-            .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+            .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedHandler)
             .and()
             .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -72,8 +75,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .cacheControl();
       
     }
-    
-    
     
     
     
