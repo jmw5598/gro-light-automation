8a185722d2d68d6c3ecd8855b6d2e333bbdb9e61
Jason White <jmw5598@gmail.com>
2017-03-06 17:33:27 -0500

    created login form component

---
+++ frontend/src/app/login/login-form/login-form.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { LoginFormComponent } from './login-form.component';
+
+describe('LoginFormComponent', () => {
+  let component: LoginFormComponent;
+  let fixture: ComponentFixture<LoginFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/login/login-form/login-form.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../shared/service/authentication/authentication.service';
+
+interface Credentials {
+  username: string;
+  password: string;
+}
+
+@Component({
+  selector: 'gro-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent {
+
+  form: FormGroup;
+
+  constructor(public formBuilder: FormBuilder, private auth: AuthenticationService) {
+    this.form = formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  submit(value: any) {
+    const credentials: Credentials = {
+      username : value.username,
+      password : value.password
+    };
+    console.log("LoginComponent: logging in..." + credentials.username);
+    this.auth.doLogin(credentials);
+  }
+
+}
