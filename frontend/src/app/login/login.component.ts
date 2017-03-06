d73fb7ce1e69b85c56b601dbe3159a751d936b73
Jason White <jmw5598@gmail.com>
2017-03-06 17:32:58 -0500

    relocated form to a form component

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
-	selector: 'gro-login',
-	templateUrl: './login.component.html';
-	styleUrls: ['./login.component.css'];
+  selector: 'gro-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+}
