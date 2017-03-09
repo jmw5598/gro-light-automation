1d000f073b1104e045b5bc0827b7b5224f625f54
Jason White <jmw5598@gmail.com>
2017-03-09 16:14:33 -0500

    animated the showing and hiding of navigation on successful login and logout

--- frontend/src/app/navigation/navigation.component.ts
+++ frontend/src/app/navigation/navigation.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, transition, style, animate } from '@angular/core';
 import { AuthenticationService } from '../_service/authentication/authentication.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'gro-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
+  animations: [
+    trigger('signal', [
+      state('void', style({
+        'transform' : 'translateY(-100%)',
+        'opacity' : '0'
+      })),
+      transition('* => *', animate(300))
+    ])
+  ]
 })
 export class NavigationComponent {
+
+  show: boolean;
 
   constructor(
     public auth: AuthenticationService,
@@ -17,6 +28,13 @@ export class NavigationComponent {
   logout() {
     this.auth.doLogout();
     this.router.navigate(['login']);
+  }
+
+  isShown() {
+    if(this.auth.isLoggedIn()) {
+      this.show = true;
+      return true;
+    }
   }
 
 }
