9a69e485f5325bb8d940c80de402cef5da3d7f8d
Jason White <jmw5598@gmail.com>
2017-03-13 11:17:15 -0400

    created relays panel component, relay model

---
+++ frontend/src/app/dashboard/relays/relay.model.ts
@@ -0,0 +1,6 @@
+export interface Relay {
+  id: number;
+  pinNum: number;
+  alias: string;
+  enabled: boolean;
+}
---
+++ frontend/src/app/dashboard/relays/relays.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Relay } from './relay.model';
+
+@Component({
+  selector: 'gro-relays',
+  templateUrl: './relays.component.html',
+  styleUrls: ['./relays.component.css']
+})
+export class RelaysComponent implements OnInit {
+
+  @Output()
+  onRelayToggle = new EventEmitter<Relay>();
+
+  @Input()
+  relays: Relay[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggle(relay: Relay) {
+    this.onRelayToggle.emit(relay);
+  }
+
+}
