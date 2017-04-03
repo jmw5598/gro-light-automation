ee054a0421a8df60abe956a76acef2b7e57696aa
Jason White <jmw5598@gmail.com>
2017-04-03 18:20:31 -0400

    created unidirectional relationship between TemperatureData and RPiComponent

--- backend/src/main/java/com/gro/model/TemperatureData.java
+++ backend/src/main/java/com/gro/model/TemperatureData.java
@@ -1,6 +1,7 @@
 package com.gro.model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -8,6 +9,10 @@ public class TemperatureData extends AbstractRPiComponentData {
     
     @NotNull
     private Double temperature;
+    
+    @ManyToOne
+    @NotNull
+    private RPiComponent component;
     
     public TemperatureData() {}
 
@@ -17,6 +22,14 @@ public class TemperatureData extends AbstractRPiComponentData {
 
     public void setTemperature(Double temperature) {
         this.temperature = temperature;
+    }
+    
+    public RPiComponent getComponent() {
+        return component;
+    }
+    
+    public void setComponent(RPiComponent component) {
+        this.component = component;
     }
     
 }
