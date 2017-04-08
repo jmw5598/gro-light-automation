4f7aff831328fa755505fbb726157f2fe18b6be9
Jason White <jmw5598@gmail.com>
2017-04-08 10:51:42 -0400

    updated rpicomponent controller

--- backend/src/main/java/com/gro/controller/RPiComponentController.java
+++ backend/src/main/java/com/gro/controller/RPiComponentController.java
@@ -2,6 +2,7 @@ package com.gro.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,37 +11,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gro.model.RPiComponent;
+import com.gro.repository.RPiComponentRepository;
 
 @RestController
 @RequestMapping(value="/api/component")
 public class RPiComponentController {
     
+    @Autowired
+    private RPiComponentRepository rPiComponentRepository;
+    
     @RequestMapping(method=RequestMethod.GET)
     public List<RPiComponent> getAllComponents() {
-        return null;
+        return rPiComponentRepository.findAll();
     }
     
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(method=RequestMethod.POST)
     public void postOneComponent(@RequestBody RPiComponent component) {
-        //save new component / possibly return component after save.
+        rPiComponentRepository.save(component);
     }
     
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
     public RPiComponent getOneComponent(@PathVariable("id") Integer id) {
-        return null;
+        return rPiComponentRepository.findOne(id);
     }
     
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value="/{id}", method=RequestMethod.PUT)
     public void updateOneComponent(@RequestBody RPiComponent component) {
-        //update component information
+        rPiComponentRepository.save(component);
     }
     
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
     public void deleteOneComponent(@RequestBody RPiComponent component) {
-        //delete component
+        rPiComponentRepository.delete(component);
     }
     
     
