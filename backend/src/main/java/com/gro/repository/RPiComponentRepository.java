61280bb611bb1741134983f8a538cc79466ce9d1
Jason White <jmw5598@gmail.com>
2017-04-16 11:50:50 -0400

    added caching annotations to use ehcache

--- backend/src/main/java/com/gro/repository/RPiComponentRepository.java
+++ backend/src/main/java/com/gro/repository/RPiComponentRepository.java
@@ -2,13 +2,49 @@ package com.gro.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.gro.model.RPiComponent;
 
 public interface RPiComponentRepository extends CrudRepository<RPiComponent, Integer> {
     
     @Override
+    @Cacheable(value="componentCache")
     List<RPiComponent> findAll();
+    
+    @Override
+    @Cacheable(value="componentCache")
+    RPiComponent findOne(Integer id);
 
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @CachePut(value="componentCache")
+    <S extends RPiComponent> S save(S entity);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    <S extends RPiComponent> Iterable<S> save(Iterable<S> entities);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @CacheEvict(value="componentCache")
+    void delete(Integer id);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @CacheEvict(value="componentCache")
+    void delete(RPiComponent entity);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void delete(Iterable<? extends RPiComponent> entities);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    void deleteAll();
+    
 }
