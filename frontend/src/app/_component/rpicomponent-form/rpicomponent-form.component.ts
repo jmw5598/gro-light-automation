579079e08c1e7dcb2ab8fb4f83feccdd52927b28
Jason White <jmw5598@gmail.com>
2017-04-23 17:54:22 -0400

    added select input for rpicomponent form

--- frontend/src/app/_component/rpicomponent-form/rpicomponent-form.component.ts
+++ frontend/src/app/_component/rpicomponent-form/rpicomponent-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { RPiComponentType } from '../../_model/rpicomponent-type.enum';
+import { RPiComponent } from '../../_model/rpicomponent';
 
 @Component({
   selector: 'gro-rpicomponent-form',
@@ -8,8 +10,13 @@ import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Valida
 })
 export class RPiComponentFormComponent implements OnInit {
 	
+  @Output()
+  onFormSubmit: EventEmitter<RPiComponent> = new EventEmitter<RPiComponent>();
+
   @Input()
   formSettings: Object;
+
+  RPiComponentType = RPiComponentType;
 
   redirect: string;
   form: FormGroup;
@@ -19,12 +26,17 @@ export class RPiComponentFormComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {
     this.form = formBuilder.group({
       alias: ['', Validators.required],
-      pin: ['', Validators.required]
+      pin: ['', Validators.required],
+      type: ['', Validators.required]
     });
     this.errorShown = false;
   }
 
   ngOnInit() {
+  }
+
+  submit(component: RPiComponent) {
+    this.onFormSubmit.emit(component);
   }
 
 }
