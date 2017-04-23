f21a9bed32532c5712818b4998fef24647c124e6
Jason White <jmw5598@gmail.com>
2017-04-23 13:37:02 -0400

    removed un-used code

--- frontend/src/app/settings/settings.component.ts
+++ frontend/src/app/settings/settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SettingsNavigationComponent } from './settings-navigation/settings-navigation.component';
+
 
 @Component({
   selector: 'gro-settings',
@@ -8,17 +8,9 @@ import { SettingsNavigationComponent } from './settings-navigation/settings-navi
 })
 export class SettingsComponent implements OnInit {
 
-  view: string;
-
-  constructor() {
-    this.view = 'general';
-  }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onViewChange(view: string): void {
-    this.view = view;
   }
 
 }
