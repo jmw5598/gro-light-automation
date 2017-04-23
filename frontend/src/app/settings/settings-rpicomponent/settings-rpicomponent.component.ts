bc13f6a1fcfa27dabca5d3561a97b5dff913f222
Jason White <jmw5598@gmail.com>
2017-04-23 13:35:45 -0400

    updated settings-rpicomponent view with a list a rpicomponents

--- frontend/src/app/settings/settings-rpicomponent/settings-rpicomponent.component.ts
+++ frontend/src/app/settings/settings-rpicomponent/settings-rpicomponent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RPICOMPONENTS, RPiComponent } from '../../_model/rpicomponent';
 
 @Component({
   selector: 'gro-settings-rpicomponent',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings-rpicomponent.component.css']
 })
 export class SettingsRPiComponentComponent implements OnInit {
+	
+  components: RPiComponent[] = RPICOMPONENTS;
 
   constructor() { }
 
