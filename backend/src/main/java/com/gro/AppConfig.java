c4788a3ecf88b6a224e6042d0418d370156e69a8
Jason White <jmw5598@gmail.com>
2017-04-25 17:16:13 -0400

    updated cors registry

--- backend/src/main/java/com/gro/AppConfig.java
+++ backend/src/main/java/com/gro/AppConfig.java
@@ -4,31 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.gro.resources.ObjectMapperFactory;
 
 @Configuration
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
     
     @Bean
     public Jackson2JsonObjectMapper jackson2JsonObjectMapper() {
         return ObjectMapperFactory.getMapper();
     }
     
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200");
-                registry.addMapping("/auth")
-                        .allowedOrigins("http://localhost:4200");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*");
     }
     
 }
