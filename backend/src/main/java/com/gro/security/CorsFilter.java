a9ebc67de15eb80261013356bb45c9838be108d8
Jason White <jmw5598@gmail.com>
2017-04-25 17:17:24 -0400

    created CorsFilter to allow Angular 2 preflight OPTIONS request

---
+++ backend/src/main/java/com/gro/security/CorsFilter.java
@@ -0,0 +1,37 @@
+package com.gro.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+public class CorsFilter extends OncePerRequestFilter {
+    
+    static final String ORIGIN = "Origin";
+
+    protected void doFilterInternal(
+            HttpServletRequest request, 
+            HttpServletResponse response, 
+            FilterChain filterChain) throws ServletException, IOException {
+        
+        String origin = request.getHeader(ORIGIN);
+        
+        response.setHeader("Access-Control-Allow-Origin", "*");//* or origin as u prefer
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "content-type, authorization");
+        
+        if (request.getMethod().equals("OPTIONS"))
+            response.setStatus(HttpServletResponse.SC_OK);
+        else 
+            filterChain.doFilter(request, response);
+        
+    }
+    
+}
