bf17b33422f06b10cf0c9a2de0fca98e7d0cf5ce
Jason White <jmw5598@gmail.com>
2017-04-26 19:12:02 -0400

    wired in delete functionality to delete components

--- frontend/src/app/settings/settings-rpicomponent/settings-rpicomponent.component.ts
+++ frontend/src/app/settings/settings-rpicomponent/settings-rpicomponent.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { RPICOMPONENTS, RPiComponent } from '../../_model/rpicomponent';
+import { RPiComponent } from '../../_model/rpicomponent';
+import { RPiComponentService } from '../../_service/rpicomponent/rpicomponent.service';
 
 @Component({
   selector: 'gro-settings-rpicomponent',
@@ -7,12 +8,32 @@ import { RPICOMPONENTS, RPiComponent } from '../../_model/rpicomponent';
   styleUrls: ['./settings-rpicomponent.component.css']
 })
 export class SettingsRPiComponentComponent implements OnInit {
-	
-  components: RPiComponent[] = RPICOMPONENTS;
 
-  constructor() { }
+  components: RPiComponent[];
+
+  constructor(private rPiComponentService: RPiComponentService) { }
 
   ngOnInit() {
+    this.loadComponents();
+  }
+
+  onComponentDelete(id: number) {
+    console.log("deleting component: " + id);
+    this.rPiComponentService
+      .delete(id)
+        .subscribe(
+          data => this.loadComponents(),
+          error => console.log(error)
+        );
+  }
+
+  private loadComponents() {
+    this.rPiComponentService
+      .findAll()
+        .subscribe(
+          data => this.components = data,
+          error => console.log('Error: ' + error)  // DISPLAY ERROR IN TABLE BODY
+        );
   }
 
 }
