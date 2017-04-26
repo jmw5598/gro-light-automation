a5ec5d4f41b0bd1368f096b66ce97535ab5802ba
Jason White <jmw5598@gmail.com>
2017-04-26 19:33:02 -0400

    created function to get relay components

--- frontend/src/app/_service/rpicomponent/rpicomponent.service.ts
+++ frontend/src/app/_service/rpicomponent/rpicomponent.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { CrudService } from '../crud.service';
 import { RPiComponent } from '../../_model/rpicomponent';
-import { AuthHttp } from 'angular2-jwt';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { RPiComponentType } from '../../_model/rpicomponent-type.enum';
 
 @Injectable()
 export class RPiComponentService extends CrudService<RPiComponent, number> {
-
+  
   constructor(http: Http) {
-  	super('http://localhost:8080/api/component', http);
+    super('http://localhost:8080/api/component', http);
+  }
+
+  findAllByType(type: RPiComponentType) {
+    return this.http.get(this.base + '/relays', this.optionstemp())
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  // this need to be refactored
+  private optionstemp() {
+    let headers = new Headers({ 'Authorization': '' + localStorage.getItem('token') });
+    let options = new RequestOptions({ headers: headers });
+    return options;
   }
 
 }
