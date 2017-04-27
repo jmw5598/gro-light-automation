62bf03f415a84e7507eca867b7d054bae968cdcb
Jason White <jmw5598@gmail.com>
2017-04-27 14:32:24 -0400

    created invalid component type exception

---
+++ backend/src/main/java/com/gro/model/InvalidRPiComponentTypeException.java
@@ -0,0 +1,9 @@
+package com.gro.model;
+
+public class InvalidRPiComponentTypeException extends RuntimeException {
+
+    public InvalidRPiComponentTypeException(String message) {
+        super(message);
+    }
+    
+}
