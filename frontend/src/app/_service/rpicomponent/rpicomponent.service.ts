20e47b291e54e397b78c470b47a126f85c53f659
Jason White <jmw5598@gmail.com>
2017-04-27 14:41:29 -0400

    updated function to get components by type

--- frontend/src/app/_service/rpicomponent/rpicomponent.service.ts
+++ frontend/src/app/_service/rpicomponent/rpicomponent.service.ts
@@ -3,6 +3,9 @@ import { CrudService } from '../crud.service';
 import { RPiComponent } from '../../_model/rpicomponent';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { RPiComponentType } from '../../_model/rpicomponent-type.enum';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RPiComponentService extends CrudService<RPiComponent, number> {
@@ -11,8 +14,10 @@ export class RPiComponentService extends CrudService<RPiComponent, number> {
     super('http://localhost:8080/api/component', http);
   }
 
-  findAllByType(type: RPiComponentType) {
-    return this.http.get(this.base + '/relays', this.optionstemp())
+
+  findAllByType(type: RPiComponentType): Observable<RPiComponent[]> {
+    console.log('type: ' + RPiComponentType[type]);
+    return this.http.get(this.base + '/byType?type=' + RPiComponentType[type], this.optionstemp())
       .map(this.extractData)
       .catch(this.handleError);
   }
