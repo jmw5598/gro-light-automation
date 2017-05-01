b60b7be9349b759bda798d43eb5f653b551080aa
Jason White <jmw5598@gmail.com>
2017-04-30 20:59:15 -0400

    updated app module after refactoring

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,54 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AuthenticationModule } from './_service/authentication/authentication.module';
-import { AppRoutingModule, routingComponents, routingGuards } from './app.routing';
-import { Ng2PageTransitionModule } from 'ng2-page-transition';
 import { UiSwitchModule } from 'angular2-ui-switch';
 import { ChartsModule } from 'ng2-charts';
 
-import { AppComponent } from './app.component';
-import { NavigationComponent} from './navigation/navigation.component';
-import { LoginFormComponent } from './login/login-form/login-form.component';
-import { AlertComponent } from './alert/alert.component';
+import { AppRoutingModule, routingComponents, routingGuards } from './app.routing';
 
-import { AuthenticationService } from './_service/authentication/authentication.service';
-import { UnauthorizedComponent } from './_component/unauthorized/unauthorized.component';
-import { SettingsNavigationComponent } from './settings/settings-navigation/settings-navigation.component';
-import { SettingsGeneralComponent } from './settings/settings-general/settings-general.component';
-import { SettingsSensorComponent } from './settings/settings-sensor/settings-sensor.component';
-import { SettingsRelayComponent } from './settings/settings-relay/settings-relay.component';
-import { PanelListGroupComponent } from './_component/panel-list-group/panel-list-group.component';
-import { RelaysComponent } from './dashboard/relays/relays.component';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { CoreModule } from './core/core.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LoginModule } from './login/login.module';
+import { NavigationModule } from './navigation/navigation.module';
+import { SettingsModule } from './settings/settings.module';
+
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginFormComponent,
-    NavigationComponent,
-    AlertComponent,
     routingComponents,
-    UnauthorizedComponent,
-    SettingsNavigationComponent,
-    SettingsGeneralComponent,
-    SettingsSensorComponent,
-    SettingsRelayComponent,
-    PanelListGroupComponent,
-    RelaysComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
-    AuthenticationModule,
     AppRoutingModule,
-    Ng2PageTransitionModule,
-    UiSwitchModule,
-    ChartsModule
+    AuthenticationModule,
+    BrowserModule,
+    ChartsModule,
+    CoreModule,
+    DashboardModule,
+    FormsModule,
+    HttpModule,
+    LoginModule,
+    NavigationModule,
+    ReactiveFormsModule,
+    SettingsModule,
+    UiSwitchModule
   ],
   providers: [
-    AuthenticationService,
     routingGuards
   ],
   bootstrap: [AppComponent]
