5127d868ab1305f85e2b737b9ee8e82faed0aaf1
Jason White <jmw5598@gmail.com>
2017-05-01 18:35:43 -0400

    added table and pageable table components to core module

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { RPiComponentService } from './service/rpicomponent/rpicomponent.service';
+import { TemperatureDataService } from './service/temperature-data/temperature-data.service';
 
 import { AlertComponent } from './component/alert/alert.component';
 import { PanelListGroupComponent } from './component/panel-list-group/panel-list-group.component';
 import { RPiComponentFormComponent } from './component/rpicomponent-form/rpicomponent-form.component';
+import { TableComponent } from './component/table/table.component';
 import { UnauthorizedComponent } from './component/unauthorized/unauthorized.component';
+import { PageableTableComponent } from './component/pageable-table/pageable-table.component';
 
 @NgModule({
   imports: [
@@ -15,18 +18,23 @@ import { UnauthorizedComponent } from './component/unauthorized/unauthorized.com
   ],
   exports: [
     AlertComponent,
+    PageableTableComponent,
     PanelListGroupComponent,
     RPiComponentFormComponent,
-    UnauthorizedComponent
+    UnauthorizedComponent,
+    TableComponent
   ],
   declarations: [
     AlertComponent,
     PanelListGroupComponent,
     RPiComponentFormComponent,
-    UnauthorizedComponent
+    UnauthorizedComponent,
+    TableComponent,
+    PageableTableComponent
   ],
   providers: [
-    RPiComponentService
+    RPiComponentService,
+    TemperatureDataService
   ]
 })
 export class CoreModule { }
