f02a2d92bcb2ed6c7c04cb83394fac47c81c1d89
Jason White <jmw5598@gmail.com>
2017-05-13 10:50:29 -0400

    created Time and Temperature Unit enums

---
+++ backend/src/main/java/com/gro/model/TemperatureUnit.java
@@ -0,0 +1,6 @@
+package com.gro.model;
+
+public enum TemperatureUnit {
+    CELSIUS,
+    FAHRENHEIT
+}
---
+++ backend/src/main/java/com/gro/model/TimeUnit.java
@@ -0,0 +1,8 @@
+package com.gro.model;
+
+public enum TimeUnit {
+    MILLISECONDS,
+    SECONDS,
+    MINUTES,
+    HOURS
+}
