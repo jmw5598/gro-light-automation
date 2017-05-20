baf08f38449f960a2028943fd03c65333919595e
Jason White <jmw5598@gmail.com>
2017-05-20 10:30:36 -0400

    created event emitter controller to emit service side event

---
+++ backend/src/main/java/com/gro/controller/EventEmitterController.java
@@ -0,0 +1,38 @@
+package com.gro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.gro.web.service.HumidityDataService;
+import com.gro.web.service.TemperatureDataService;
+
+@RestController
+@RequestMapping("/api/event")
+public class EventEmitterController {
+    
+    @Autowired
+    private TemperatureDataService temperatureDataService;
+    
+    @Autowired
+    private HumidityDataService humidityDataService;
+    
+    @RequestMapping("/temperature")
+    public SseEmitter streamTemperature() {
+        SseEmitter emitter = new SseEmitter();
+        emitter.onCompletion(() -> this.temperatureDataService.removeEmitter(emitter));
+        this.temperatureDataService.addEmitter(emitter);
+        return emitter;
+    }
+    
+    @RequestMapping("/humidity")
+    public SseEmitter streamHumidity() {
+        SseEmitter emitter = new SseEmitter();
+        emitter.onCompletion(() -> this.humidityDataService.removeEmitter(emitter));
+        this.humidityDataService.addEmitter(emitter);
+        return emitter;
+        
+    }
+    
+}
