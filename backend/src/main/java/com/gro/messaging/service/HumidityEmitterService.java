a3a89c5f3217e566de349c61cac69c2366473a30
Jason White <jmw5598@gmail.com>
2017-05-20 10:32:45 -0400

    Renamed socket services to emitter. Emits new temps and humidity when receiving new mqtt message

--- backend/src/main/java/com/gro/messaging/service/HumidityEmitterService.java
+++ backend/src/main/java/com/gro/messaging/service/HumidityEmitterService.java
@@ -1,18 +1,22 @@
 package com.gro.messaging.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 
 import com.gro.model.HumidityDTO;
+import com.gro.web.service.HumidityDataService;
 
 @MessageEndpoint
-public class HumiditySocketService {
+public class HumidityEmitterService {
+    
+    @Autowired
+    private HumidityDataService humidityDataService;
     
     @ServiceActivator(inputChannel="humidityServiceChannel")
     public void process(Message<HumidityDTO> message) {
-        //publish humidity change to websocket topic to update angular 2
-        System.out.println("updating humidity socket");
+        humidityDataService.emit(message.getPayload());
     }
     
 }
--- backend/src/main/java/com/gro/messaging/service/TemperatureEmitterService.java
+++ backend/src/main/java/com/gro/messaging/service/TemperatureEmitterService.java
@@ -1,18 +1,22 @@
 package com.gro.messaging.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 
 import com.gro.model.TemperatureDTO;
+import com.gro.web.service.TemperatureDataService;
 
 @MessageEndpoint
-public class TemperatureSocketService {
+public class TemperatureEmitterService {
+    
+    @Autowired
+    private TemperatureDataService temperatureDataService;
     
     @ServiceActivator(inputChannel="temperatureServiceChannel")
     public void process(Message<TemperatureDTO> message) {
-        //publish temperature change to websocket topic to update angular 2
-        System.out.println("updating temperature socket");
+        temperatureDataService.emit(message.getPayload());
     }
     
 }
