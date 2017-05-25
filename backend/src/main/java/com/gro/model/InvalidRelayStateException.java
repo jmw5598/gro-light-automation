b2682fd5e78a0567504846d683d2faa366f059ff
Jason White <jmw5598@gmail.com>
2017-05-25 14:14:28 -0400

    created exception for invalid relay state

---
+++ backend/src/main/java/com/gro/model/InvalidRelayStateException.java
@@ -0,0 +1,9 @@
+package com.gro.model;
+
+public class InvalidRelayStateException extends RuntimeException {
+    
+    public InvalidRelayStateException(String message) {
+        super(message);
+    }
+
+}
