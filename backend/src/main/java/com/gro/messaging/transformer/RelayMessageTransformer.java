8bbf0c926d86fc97b9710019645f888e0fa9937a
Jason White <jmw5598@gmail.com>
2017-05-25 14:13:44 -0400

    created relay message transformer

---
+++ backend/src/main/java/com/gro/messaging/transformer/RelayMessageTransformer.java
@@ -0,0 +1,26 @@
+package com.gro.messaging.transformer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import com.gro.model.RelayDTO;
+
+@MessageEndpoint
+public class RelayMessageTransformer {
+    
+    @Autowired
+    private Jackson2JsonObjectMapper jackson2JsonObjectMapper;
+    
+    @Transformer(inputChannel="relayTransformerChannel",
+                 outputChannel="relayEmitterChannel")
+    public Message<RelayDTO> transform(Message<String> message) throws Exception {
+        String payload = message.getPayload();
+        RelayDTO data = jackson2JsonObjectMapper.fromJson(payload, RelayDTO.class);
+        return MessageBuilder.createMessage(data, message.getHeaders());
+    }
+    
+}
