2b72efbd2896de927acc516e447d5059dddefec5
Jason White <jmw5598@gmail.com>
2017-05-25 14:18:06 -0400

    updated messaging config

--- backend/src/main/java/com/gro/messaging/MessagingConfig.java
+++ backend/src/main/java/com/gro/messaging/MessagingConfig.java
@@ -2,14 +2,17 @@ package com.gro.messaging;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 
 
 @Configuration
@@ -56,6 +59,19 @@ public class MessagingConfig {
         return new DirectChannel();
     }
     
+    /* RELAY : CHANNEL BETWEEN TRANSFORMER AND SERVICE */
+    @Bean
+    public MessageChannel relayEmitterChannel() {
+        return new DirectChannel();
+    }
+    
+    /* RELAY : CHANNEL BETWEEN ROUTER AND TRANSFORMER */
+    @Bean
+    public MessageChannel relayTransformerChannel() {
+        return new DirectChannel();
+    }
+    
+    
     @Bean
     public MqttPahoClientFactory mqttClientFactory() {
         DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
@@ -68,12 +84,21 @@ public class MessagingConfig {
     @Bean
     public MessageProducer inbound() {
         MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter("testingMqtt", mqttClientFactory(), "TEMPERATURE", "HUMIDITY", "PROXIMITY");
+                new MqttPahoMessageDrivenChannelAdapter("testingMqtt", mqttClientFactory(), "TEMPERATURE", "HUMIDITY", "PROXIMITY", "RELAY");
         adapter.setCompletionTimeout(5000);
         adapter.setConverter(new DefaultPahoMessageConverter());
         adapter.setQos(1);
         adapter.setOutputChannel(mqttRouterChannel());
         return adapter;
+    }
+    
+    @Bean
+    @ServiceActivator(inputChannel="mqttOutboundChannel")
+    public MessageHandler outbound() {
+        MqttPahoMessageHandler handler = new MqttPahoMessageHandler("mqttPublisher", mqttClientFactory());
+        handler.setAsync(true);
+        handler.setDefaultTopic("TEST");
+        return handler;
     }
     
 }
