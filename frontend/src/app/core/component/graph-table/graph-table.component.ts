af4ea49c7195d99b7bf93f5a61a37af3d6f717da
Jason White <jmw5598@gmail.com>
2017-06-22 13:37:31 -0400

    initial graph-table component commit

---
+++ frontend/src/app/core/component/graph-table/graph-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-table',
+  templateUrl: './graph-table.component.html',
+  styleUrls: ['./graph-table.component.css']
+})
+export class GraphTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
