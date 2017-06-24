0ce475a3303450057eb74d4694c41ec015f73588
Jason White <jmw5598@gmail.com>
2017-06-24 09:11:48 -0400

    initial commit for graph table data menu

---
+++ frontend/src/app/core/component/graph-table/graph-table-data/graph-table-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gro-graph-table-data',
+  templateUrl: './graph-table-data.component.html',
+  styleUrls: ['./graph-table-data.component.css']
+})
+export class GraphTableDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
