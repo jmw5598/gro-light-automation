48947e0a84aec131a7d3c770c9a358737c4c022a
Jason White <jmw5598@gmail.com>
2017-06-27 18:49:56 -0400

    added functionality to graph table data dropdown

--- frontend/src/app/core/component/graph-table/graph-table-data/graph-table-data.component.ts
+++ frontend/src/app/core/component/graph-table/graph-table-data/graph-table-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'gro-graph-table-data',
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphTableDataComponent implements OnInit {
 
+  @Output()
+  onUpdateData: EventEmitter<Object> = new EventEmitter<Object>();
+
+  @Input()
+  metricDataType: string = 'temperature';
+
+  @Input()
+  metricCalc: string = 'average';
+
+  @Input()
+  metricTime: string = 'daily';
+
   constructor() { }
 
   ngOnInit() {
+    this.updateData();
+  }
+
+  changeMetricDataType(metricDataType: string) {
+    this.metricDataType = metricDataType;
+  }
+
+  changeMetricCalc(metricCalc: string) {
+    this.metricCalc = metricCalc;
+  }
+
+  updateData() {
+    let details = {
+      componentId: 1,
+      metricDataType: this.metricDataType,
+      metricTime: this.metricTime,
+      metricCalc: this.metricCalc
+    };
+    this.onUpdateData.emit(details);
   }
 
 }
