414540c9a8c9363e1239c97493a71d6052de9c48
Jason White <jmw5598@gmail.com>
2017-06-29 16:00:03 -0400

    added event source to handle incoming relay events

--- frontend/src/app/dashboard/relays/relays.component.ts
+++ frontend/src/app/dashboard/relays/relays.component.ts
@@ -1,29 +1,55 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { RelayDTO } from '../../core/model/rpicomponent/relaydto.model';
+
+import { RPiComponentService } from '../../core/service/rpicomponent/rpicomponent.service';
+import { RelayService } from '../../core/service/relay/relay.service';
+
+declare let EventSource:any;
 
 @Component({
   selector: 'gro-relays',
   templateUrl: './relays.component.html',
   styleUrls: ['./relays.component.css']
 })
-export class RelaysComponent implements OnInit {
+export class RelaysComponent implements OnInit, OnDestroy {
 
-  @Output()
-  onRelayToggle: EventEmitter<RelayDTO> = new EventEmitter<RelayDTO>();
-
-  @Input()
   relays: RelayDTO[];
+  private relayEvents;
 
-  constructor() { }
+  constructor(
+    private rPiComponentService: RPiComponentService,
+    private relayService: RelayService
+  ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.rPiComponentService
+      .findAllRelays()
+        .subscribe(
+          data => {
+            this.relays = data
+            this.relays.forEach(e => this.relayService.poll(e));
+          },
+          error => console.log("error getting relays") // replace with toast message
+        );
+
+    this.relayEvents = new EventSource('http://192.168.1.7:8080/api/event/relay');
+    this.relayEvents.addEventListener('message', message => {
+      let relay = JSON.parse(message.data);
+      let obj = this.relays.find(e => e.component.id === relay.component.id);
+      obj.state =relay.state;
+    });
+  }
+
+  ngOnDestroy() {
+    this.relayEvents.close();
+  }
 
   toggle(relay: RelayDTO) {
     if(relay.state === 'ON')
       relay.state = 'OFF'
     else if(relay.state === 'OFF')
       relay.state = 'ON';
-    this.onRelayToggle.emit(relay);
+    this.relayService.toggle(relay);
   }
 
 }
