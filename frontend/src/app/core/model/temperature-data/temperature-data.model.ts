734666681817c2ed8d73ed40570e77f4cd6bf63d
Jason White <jmw5598@gmail.com>
2017-06-29 16:13:07 -0400

    renamed Date attribute to timestamp

--- frontend/src/app/core/model/temperature-data/temperature-data.model.ts
+++ frontend/src/app/core/model/temperature-data/temperature-data.model.ts
@@ -3,7 +3,7 @@ import { RPiComponent } from '../rpicomponent/rpicomponent.model';
 export class TemperatureData {
 
   constructor(
-    public date: Date,
+    public timestamp: Date,
     public component: RPiComponent,
     public temperature: number
   ) {}
