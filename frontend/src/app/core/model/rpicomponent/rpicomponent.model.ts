5a2fbebe4e3de5057499413d19f9fb4501800d75
Jason White <jmw5598@gmail.com>
2017-06-29 16:02:08 -0400

    added current reading attribute to model

--- frontend/src/app/core/model/rpicomponent/rpicomponent.model.ts
+++ frontend/src/app/core/model/rpicomponent/rpicomponent.model.ts
@@ -2,11 +2,14 @@ import { RPiComponentType } from './rpicomponent-type.enum';
 
 export class RPiComponent {
 
+  current: number;
   constructor (
     public id: number,
     public alias: string,
     public pin: number,
     public type: RPiComponentType,
-  ) {}
+  ) {
+    this.current = 0;
+  }
 
 }
