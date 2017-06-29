dc119446ec531da697211b3f0b71a5b0e2dc6329
Jason White <jmw5598@gmail.com>
2017-06-29 16:01:12 -0400

    updated core module

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
-
 import { SharedModule } from '../shared/shared.module';
+import { ChartsModule } from 'ng2-charts';
 
+import { HumidityDataService } from './service/humidity-data/humidity-data.service';
+import { RelayService } from './service/relay/relay.service';
 import { RPiComponentService } from './service/rpicomponent/rpicomponent.service';
 import { TemperatureDataService } from './service/temperature-data/temperature-data.service';
 
@@ -11,9 +13,16 @@ import { RPiComponentFormComponent } from './component/rpicomponent-form/rpicomp
 import { TableComponent } from './component/table/table.component';
 import { UnauthorizedComponent } from './component/unauthorized/unauthorized.component';
 import { PageableTableComponent } from './component/pageable-table/pageable-table.component';
+import { KeyToTitlePipe } from './pipe/key-to-title/key-to-title.pipe';
+import { KeyExtractPipe } from './pipe/key-extract/key-extract.pipe';
+import { GraphComponent } from './component/graph/graph.component';
+import { GraphTableComponent } from './component/graph-table/graph-table.component';
+import { GraphTableOptionsComponent } from './component/graph-table/graph-table-options/graph-table-options.component';
+import { GraphTableDataComponent } from './component/graph-table/graph-table-data/graph-table-data.component';
 
 @NgModule({
   imports: [
+    ChartsModule,
     SharedModule
   ],
   exports: [
@@ -22,7 +31,9 @@ import { PageableTableComponent } from './component/pageable-table/pageable-tabl
     PanelListGroupComponent,
     RPiComponentFormComponent,
     UnauthorizedComponent,
-    TableComponent
+    TableComponent,
+    GraphComponent,
+    GraphTableComponent
   ],
   declarations: [
     AlertComponent,
@@ -30,9 +41,17 @@ import { PageableTableComponent } from './component/pageable-table/pageable-tabl
     RPiComponentFormComponent,
     UnauthorizedComponent,
     TableComponent,
-    PageableTableComponent
+    PageableTableComponent,
+    KeyToTitlePipe,
+    KeyExtractPipe,
+    GraphComponent,
+    GraphTableComponent,
+    GraphTableOptionsComponent,
+    GraphTableDataComponent,
   ],
   providers: [
+    HumidityDataService,
+    RelayService,
     RPiComponentService,
     TemperatureDataService
   ]
