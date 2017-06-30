864eb24c00bf804d5f6ff60fc62f185e9c60a39e
Jason White <jmw5598@gmail.com>
2017-06-30 18:54:47 -0400

    created celsius to fahrenheit pipe for temperature readins

--- frontend/src/app/core/pipe/celsius-to-fahrenheit/celsius-to-fahrenheit.pipe.ts
+++ frontend/src/app/core/pipe/celsius-to-fahrenheit/celsius-to-fahrenheit.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CelsiusToFahrenheitPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: number): any {
+    return (value * 1.8 + 32).toFixed(2);
   }
 
 }
