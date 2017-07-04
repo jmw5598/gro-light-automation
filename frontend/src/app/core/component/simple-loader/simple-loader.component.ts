08a6da1fc256674016b245264502c3c6a476c442
Jason White <jmw5598@gmail.com>
2017-07-04 10:38:48 -0400

    created simple loading spinner component

--- frontend/src/app/core/component/simple-loader/simple-loader.component.ts
+++ frontend/src/app/core/component/simple-loader/simple-loader.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-simple-loader',
+  selector: 'gro-simple-loader',
   templateUrl: './simple-loader.component.html',
   styleUrls: ['./simple-loader.component.css']
 })
 export class SimpleLoaderComponent implements OnInit {
+
+  @Input()
+  isLoading: boolean = false;
+
+  @Input()
+  size: string = 'medium';
 
   constructor() { }
 
