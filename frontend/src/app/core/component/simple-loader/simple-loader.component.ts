cdad3715c781254e1e8077d6eae6a9b8796819e6
Jason White <jmw5598@gmail.com>
2017-07-04 10:01:58 -0400

    initial commit for simple loader component

---
+++ frontend/src/app/core/component/simple-loader/simple-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-loader',
+  templateUrl: './simple-loader.component.html',
+  styleUrls: ['./simple-loader.component.css']
+})
+export class SimpleLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
