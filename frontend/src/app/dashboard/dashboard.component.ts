e5a322d12d1f5375e94fc4347de2f062a0e92896
Jason White <jmw5598@gmail.com>
2017-07-09 10:45:26 -0400

    added paged loader to template and extending page loading base class

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { PageLoading } from '../core/component/page-loader/page-loading';
 
 @Component({
   selector: 'gro-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent extends PageLoading implements OnInit {
 
-  constructor() { }
+  constructor() {
+    super(true);
+  }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.standby();
+    setTimeout(() => this.ready(), 1000);
+  }
 
 }
