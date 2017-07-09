00088d2dd0ba04e291d837ae64c2ff71c515fb5d
Jason White <jmw5598@gmail.com>
2017-07-09 10:34:42 -0400

    added constructor

--- frontend/src/app/core/component/page-loader/page-loading.ts
+++ frontend/src/app/core/component/page-loader/page-loading.ts
@@ -2,6 +2,10 @@ export class PageLoading {
 
   protected isLoading: boolean;
 
+  constructor(isLoading:boolean) {
+    this.isLoading = isLoading;
+  }
+
   protected standby() {
     this.isLoading = true;
   }
