116005ca38a7c880f627755f0039fba890820a3a
Jason White <jmw5598@gmail.com>
2017-07-17 13:01:28 -0400

    initial commit of dashboard-builder-toolbar

---
+++ frontend/src/app/dashboard/dashboard-builder/dashboard-builder-toolbar/dashboard-builder-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-builder-toolbar',
+  templateUrl: './dashboard-builder-toolbar.component.html',
+  styleUrls: ['./dashboard-builder-toolbar.component.css']
+})
+export class DashboardBuilderToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
