8198ebf1035da7d0c75010df6970abf1bbea6a0e
Jason White <jmw5598@gmail.com>
2017-07-17 12:53:58 -0400

    renamed dashboard-builder-toolbar to toolbar

---
+++ frontend/src/app/dashboard/dashboard-builder/toolbar/dashboard-builder-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-builder-toolbar',
+  templateUrl: './dashboard-builder-toolbar.component.html',
+  styleUrls: ['./dashboard-builder-toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
