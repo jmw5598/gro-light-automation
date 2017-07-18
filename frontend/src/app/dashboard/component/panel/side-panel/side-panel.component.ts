9439b7deed9de79b5a6f8f1c24e95e9dee480686
Jason White <jmw5598@gmail.com>
2017-07-18 09:26:03 -0400

    initial commit for dashboard side-panel component

---
+++ frontend/src/app/dashboard/component/panel/side-panel/side-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-panel',
+  templateUrl: './side-panel.component.html',
+  styleUrls: ['./side-panel.component.css']
+})
+export class SidePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
