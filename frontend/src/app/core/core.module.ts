e1c13aa5c3398f2810d16c281bebc00fdde62695
Jason White <jmw5598@gmail.com>
2017-07-21 14:30:44 -0400

    updated core.module.ts after moving components

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -10,15 +10,9 @@ import { TemperatureDataService } from './service/temperature-data/temperature-d
 import { AlertComponent } from './component/alert/alert.component';
 import { PanelListGroupComponent } from './component/panel-list-group/panel-list-group.component';
 import { RPiComponentFormComponent } from './component/rpicomponent-form/rpicomponent-form.component';
-import { TableComponent } from './component/table/table.component';
 import { UnauthorizedComponent } from './component/unauthorized/unauthorized.component';
-import { PageableTableComponent } from './component/pageable-table/pageable-table.component';
 import { KeyToTitlePipe } from './pipe/key-to-title/key-to-title.pipe';
 import { KeyExtractPipe } from './pipe/key-extract/key-extract.pipe';
-import { GraphComponent } from './component/graph/graph.component';
-import { GraphTableComponent } from './component/graph-table/graph-table.component';
-import { GraphTableOptionsComponent } from './component/graph-table/graph-table-options/graph-table-options.component';
-import { GraphTableDataComponent } from './component/graph-table/graph-table-data/graph-table-data.component';
 import { CelsiusToFahrenheitPipe } from './pipe/celsius-to-fahrenheit/celsius-to-fahrenheit.pipe';
 import { SimpleLoaderComponent } from './component/simple-loader/simple-loader.component';
 import { PageLoaderComponent } from './component/page-loader/page-loader.component';
@@ -30,13 +24,9 @@ import { PageLoaderComponent } from './component/page-loader/page-loader.compone
   ],
   exports: [
     AlertComponent,
-    PageableTableComponent,
     PanelListGroupComponent,
     RPiComponentFormComponent,
     UnauthorizedComponent,
-    TableComponent,
-    GraphComponent,
-    GraphTableComponent,
     CelsiusToFahrenheitPipe,
     SimpleLoaderComponent,
     PageLoaderComponent
@@ -46,14 +36,8 @@ import { PageLoaderComponent } from './component/page-loader/page-loader.compone
     PanelListGroupComponent,
     RPiComponentFormComponent,
     UnauthorizedComponent,
-    TableComponent,
-    PageableTableComponent,
     KeyToTitlePipe,
     KeyExtractPipe,
-    GraphComponent,
-    GraphTableComponent,
-    GraphTableOptionsComponent,
-    GraphTableDataComponent,
     CelsiusToFahrenheitPipe,
     SimpleLoaderComponent,
     PageLoaderComponent
