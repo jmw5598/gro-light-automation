3aa771303536b8ec52318dd0277bcf0d8151ceb7
Jason White <jmw5598@gmail.com>
2017-07-21 17:37:58 -0400

    initial commit for simple reading component

---
+++ frontend/src/app/dashboard/component/simple-reading/simple-reading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-reading',
+  templateUrl: './simple-reading.component.html',
+  styleUrls: ['./simple-reading.component.css']
+})
+export class SimpleReadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
