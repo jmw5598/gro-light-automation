d4be32bb15abcb94658355ca2096550cb8511ee3
Jason White <jmw5598@gmail.com>
2017-07-22 18:50:44 -0400

    added subscriptions for proximity and moisture events

--- frontend/src/app/core/service/sse/sse.service.ts
+++ frontend/src/app/core/service/sse/sse.service.ts
@@ -9,10 +9,14 @@ export class SseService implements OnDestroy {
   private relay = new Subject<Object>();
   private humidity = new Subject<Object>();
   private temperature = new Subject<Object>();
+  private proximity = new Subject<Object>();
+  private moisture = new Subject<Object>();
 
   public relayState = this.relay.asObservable();
   public humidityState = this.humidity.asObservable();
   public temperatureState = this.temperature.asObservable();
+  public proximityState = this.proximity.asObservable();
+  public moistureState = this.moisture.asObservable();
 
   private events;
 
@@ -32,6 +36,12 @@ export class SseService implements OnDestroy {
           break;
         case 'relay':
           this.relay.next(json.payload)
+          break;
+        case 'proximity':
+          this.proximity.next(json.payload);
+          break;
+        case 'moisture':
+          this.moisture.next(json.payload);
           break;
         default:
           break;
