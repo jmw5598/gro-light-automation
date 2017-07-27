5c5bed80cfb3ed5176b8380798207e5940c28a49
Jason White <jmw5598@gmail.com>
2017-07-27 15:20:01 -0400

    updated module imports, declarations, exports after refactoring

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -6,43 +6,12 @@ import { RelayService } from './service/relay/relay.service';
 import { RPiComponentService } from './service/rpicomponent/rpicomponent.service';
 import { TemperatureDataService } from './service/temperature-data/temperature-data.service';
 
-import { AlertComponent } from './component/alert/alert.component';
-import { PanelListGroupComponent } from './component/panel-list-group/panel-list-group.component';
-import { RPiComponentFormComponent } from './component/rpicomponent-form/rpicomponent-form.component';
-import { UnauthorizedComponent } from './component/unauthorized/unauthorized.component';
-import { KeyToTitlePipe } from './pipe/key-to-title/key-to-title.pipe';
-import { KeyExtractPipe } from './pipe/key-extract/key-extract.pipe';
-import { CelsiusToFahrenheitPipe } from './pipe/celsius-to-fahrenheit/celsius-to-fahrenheit.pipe';
-import { SimpleLoaderComponent } from './component/simple-loader/simple-loader.component';
-import { PageLoaderComponent } from './component/page-loader/page-loader.component';
 import { SseService } from './service/sse/sse.service';
 
 @NgModule({
-  imports: [
-    SharedModule
-  ],
-  exports: [
-    AlertComponent,
-    PanelListGroupComponent,
-    RPiComponentFormComponent,
-    UnauthorizedComponent,
-    CelsiusToFahrenheitPipe,
-    SimpleLoaderComponent,
-    PageLoaderComponent,
-    KeyToTitlePipe,
-    KeyExtractPipe
-  ],
-  declarations: [
-    AlertComponent,
-    PanelListGroupComponent,
-    RPiComponentFormComponent,
-    UnauthorizedComponent,
-    KeyToTitlePipe,
-    KeyExtractPipe,
-    CelsiusToFahrenheitPipe,
-    SimpleLoaderComponent,
-    PageLoaderComponent
-  ],
+  imports: [],
+  exports: [],
+  declarations: [],
   providers: [
     HumidityDataService,
     RelayService,
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -4,13 +4,45 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UiSwitchModule } from 'angular2-ui-switch';
 
+import { AlertComponent } from './component/alert/alert.component';
+import { PanelListGroupComponent } from './component/panel-list-group/panel-list-group.component';
+import { RPiComponentFormComponent } from './component/rpicomponent-form/rpicomponent-form.component';
+import { UnauthorizedComponent } from './component/unauthorized/unauthorized.component';
+import { KeyToTitlePipe } from './pipe/key-to-title/key-to-title.pipe';
+import { KeyExtractPipe } from './pipe/key-extract/key-extract.pipe';
+import { CelsiusToFahrenheitPipe } from './pipe/celsius-to-fahrenheit/celsius-to-fahrenheit.pipe';
+import { SimpleLoaderComponent } from './component/simple-loader/simple-loader.component';
+import { PageLoaderComponent } from './component/page-loader/page-loader.component';
+
 @NgModule({
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
   exports: [
-  	CommonModule,
-  	FormsModule,
+    AlertComponent,
+    CelsiusToFahrenheitPipe,
+    KeyToTitlePipe,
+    KeyExtractPipe,
+    PageLoaderComponent,
+    PanelListGroupComponent,
   	ReactiveFormsModule,
+    RPiComponentFormComponent,
   	RouterModule,
-  	UiSwitchModule
+    SimpleLoaderComponent,
+  	UiSwitchModule,
+    UnauthorizedComponent
+  ],
+  declarations: [
+    AlertComponent,
+    CelsiusToFahrenheitPipe,
+    KeyToTitlePipe,
+    KeyExtractPipe,
+    PageLoaderComponent,
+    PanelListGroupComponent,
+    RPiComponentFormComponent,
+    SimpleLoaderComponent,
+    UnauthorizedComponent
   ]
 })
 export class SharedModule { }
