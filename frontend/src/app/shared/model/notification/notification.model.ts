78e12ecddd4106e887bdcaf2de82f7f9c0bb6ccd
Jason White <jmw5598@gmail.com>
2017-08-02 07:03:01 -0400

    created Notification model

---
+++ frontend/src/app/shared/model/notification/notification.model.ts
@@ -0,0 +1,12 @@
+import { RPiComponent } from '@app/shared/model/rpicomponent/rpicomponent.model';
+
+export class Notification {
+
+  constructor(
+    public id: number,
+    public timestamp: Date,
+    public message: string,
+    public component: RPiComponent
+  ) {}
+
+}
