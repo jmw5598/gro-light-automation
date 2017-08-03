9cc0188f7713fc2c63da972365f869641775145a
Jason White <jmw5598@gmail.com>
2017-08-03 14:50:15 -0400

    updated navigation templete to use notifications component & updated navigation module

--- frontend/src/app/navigation/navigation.module.ts
+++ frontend/src/app/navigation/navigation.module.ts
@@ -4,12 +4,13 @@ import { SharedModule } from '@app/shared/shared.module';
 
 import { NavigationComponent } from './navigation.component';
 import { NotificationDropdownComponent } from './notification-dropdown/notification-dropdown.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
   exports: [NavigationComponent],
-  declarations: [NavigationComponent, NotificationDropdownComponent]
+  declarations: [NavigationComponent, NotificationDropdownComponent, NotificationComponent]
 })
 export class NavigationModule {}
