ed2750eb5ebe41351b2a879b7d6568e01c069e2c
Jason White <jmw5598@gmail.com>
2017-08-03 15:08:53 -0400

    created notification crud service

--- frontend/src/app/core/service/notification/notification.service.ts
+++ frontend/src/app/core/service/notification/notification.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { CrudService } from '@app/core/service/crud.service';
+
+import { Notification } from '@app/shared/model/notification/notification.model';
 
 @Injectable()
-export class NotificationService {
+export class NotificationService extends CrudService<Notification, number> {
 
-  constructor() { }
+  constructor(http: Http) {
+    super('http://192.168.1.7:8080/api/component', http);
+  }
 
 }
