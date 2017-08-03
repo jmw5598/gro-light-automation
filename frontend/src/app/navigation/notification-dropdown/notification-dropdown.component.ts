297bb332412f78a42bd862984a819992cf59bb24
Jason White <jmw5598@gmail.com>
2017-08-03 14:48:48 -0400

    implemented notifications dropdown menu for navbar

--- frontend/src/app/navigation/notification-dropdown/notification-dropdown.component.ts
+++ frontend/src/app/navigation/notification-dropdown/notification-dropdown.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Notification } from '@app/shared/model/notification/notification.model';
+import { SseService } from '@app/core/service/sse/sse.service';
 
 @Component({
-  selector: 'app-notification-dropdown',
+  selector: 'gro-notification-dropdown',
   templateUrl: './notification-dropdown.component.html',
   styleUrls: ['./notification-dropdown.component.css']
 })
-export class NotificationDropdownComponent implements OnInit {
+export class NotificationDropdownComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private subscription;
+  private notifications: Array<Notification>;
+
+  constructor(private sseService: SseService) {
+    this.notifications = new Array<Notification>();
+  }
 
   ngOnInit() {
+    this.subscription = this.sseService
+      .notificationAlert
+        .subscribe(
+          notification => this.handleNotification(notification)
+        );
+  }
+
+  onDeleteNotification(notification: Notification) {
+    let index = this.notifications.indexOf(notification);
+    if(index > -1)
+      this.notifications.splice(index, 1);
+  }
+
+  private handleNotification(object) {
+    let notification: Notification =
+        new Notification(object.id, new Date(object.timestamp), object.message, object.component);
+    this.notifications.unshift(notification);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
