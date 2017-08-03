dff4352689c27cca4150c306c6bf4dfd8ae94d17
Jason White <jmw5598@gmail.com>
2017-08-03 14:49:30 -0400

    implemented single notification component

--- frontend/src/app/navigation/notification/notification.component.ts
+++ frontend/src/app/navigation/notification/notification.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Notification } from '@app/shared/model/notification/notification.model';
 
 @Component({
-  selector: 'app-notification',
+  selector: 'gro-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
 export class NotificationComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  notification: Notification;
+
+  @Output()
+  onDeleteNotification: EventEmitter<Notification> = new EventEmitter<Notification>();
+
+  private detailsShown: boolean;
+
+  constructor() {
+    this.detailsShown = false;
+  }
 
   ngOnInit() {
+  }
+
+  toggleDetails() {
+    this.detailsShown = !this.detailsShown;
+  }
+
+  delete(notification: Notification) {
+    this.onDeleteNotification.emit(notification);
   }
 
 }
