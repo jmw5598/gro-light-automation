37e587a103d6c75c03c34e62ab37b4232e972cb8
Jason White <jmw5598@gmail.com>
2017-08-03 15:37:07 -0400

    created dashboard crud service

--- frontend/src/app/core/service/dashboard.service.ts
+++ frontend/src/app/core/service/dashboard.service.ts
@@ -7,7 +7,7 @@ import { Dashboard } from '@app/shared/model/dashboard/dashboard.model';
 @Injectable()
 export class DashboardService extends CrudService<Dashboard, number> {
 
-  constructor(http: Http) {
+  constructor(http: Http ) {
     super('http://192.168.1.7:8080/api/component', http);
   }
 
