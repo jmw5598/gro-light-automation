5e8bff84c6e56b914feeb5b7c0d55cf077cbfc50
Jason White <jmw5598@gmail.com>
2017-08-05 10:35:13 -0400

    created notification not found exception

---
+++ backend/src/main/java/com/gro/model/NotificationNotFoundException.java
@@ -0,0 +1,8 @@
+package com.gro.model;
+
+public class NotificationNotFoundException extends RuntimeException {
+    
+    public NotificationNotFoundException(String message) {
+        super(message);
+    }
+}
