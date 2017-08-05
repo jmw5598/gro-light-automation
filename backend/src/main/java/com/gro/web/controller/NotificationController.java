ae14a935ad113cba7c5f4eb1bb1aebd7076e84dc
Jason White <jmw5598@gmail.com>
2017-08-05 10:35:49 -0400

    created notfication controller

---
+++ backend/src/main/java/com/gro/web/controller/NotificationController.java
@@ -0,0 +1,48 @@
+package com.gro.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gro.repository.NotificationRepository;
+import com.gro.model.Notification;
+import com.gro.model.NotificationNotFoundException;
+
+@RestController("/api/notification")
+public class NotificationController {
+    
+    @Value("${notification-not-found}")
+    private String notificationNotFound;
+    
+    @Autowired
+    private NotificationRepository notificationRepository;
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public Page<Notification> getAllNotifications(
+            @PageableDefault(sort={"timestamp"}, page=0, size=10) Pageable pageable) {
+        return notificationRepository.findAll(pageable);
+    }
+    
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @RequestMapping(method=RequestMethod.POST)
+    public Notification postNotification(@RequestBody Notification notification) {
+        return this.notificationRepository.save(notification);
+    }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public Notification getNotificationById(@PathVariable("id") Integer id) {
+        Notification notification = this.notificationRepository.findOne(id);
+        if(notification == null)
+            throw new NotificationNotFoundException(notificationNotFound);
+        return notification;
+    }
+
+}
