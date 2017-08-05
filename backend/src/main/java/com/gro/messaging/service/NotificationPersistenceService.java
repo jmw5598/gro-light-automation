5688edb012cceeedc772a135ae73456be1538929
Jason White <jmw5598@gmail.com>
2017-08-05 10:33:12 -0400

    created notification message endpoint to persiste messages to database

---
+++ backend/src/main/java/com/gro/messaging/service/NotificationPersistenceService.java
@@ -0,0 +1,22 @@
+package com.gro.messaging.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+
+import com.gro.model.Notification;
+import com.gro.repository.NotificationRepository;
+
+@MessageEndpoint
+public class NotificationPersistenceService {
+    
+    @Autowired
+    private NotificationRepository notificationRepository;
+    
+    @ServiceActivator(inputChannel="notificationServiceChannel")
+    public void process(Message<Notification> message) {
+        this.notificationRepository.save(message.getPayload());
+    }
+
+}
