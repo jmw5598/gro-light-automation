354d5700210b5ced430038775eeacd414f7645b9
Jason White <jmw5598@gmail.com>
2017-08-05 10:33:53 -0400

    created Notification model entity

---
+++ backend/src/main/java/com/gro/model/Notification.java
@@ -0,0 +1,67 @@
+package com.gro.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Notification {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
+    private Date timestamp;
+    
+    @NotNull
+    private String message;
+    
+    @ManyToOne
+    @NotNull
+    private RPiComponent component;
+    
+    
+    public Notification() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public RPiComponent getComponent() {
+        return component;
+    }
+
+    public void setComponent(RPiComponent component) {
+        this.component = component;
+    }    
+    
+}
