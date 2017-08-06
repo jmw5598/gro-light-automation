49e334feef8e0b779688b3f0b32fcf40471ea145
Jason White <jmw5598@gmail.com>
2017-08-06 17:49:41 -0400

    add query method to get notification by read boolean flag

--- backend/src/main/java/com/gro/repository/NotificationRepository.java
+++ backend/src/main/java/com/gro/repository/NotificationRepository.java
@@ -3,11 +3,14 @@ package com.gro.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.gro.model.notification.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     
-    public Page<Notification> findAllByHasRead(Boolean read, Pageable pageable);
+    @Query("SELECT n FROM Notification n WHERE isRead = :read")
+    public Page<Notification> findAllByIsRead(@Param("read") Boolean read, Pageable pageable);
 
 }
