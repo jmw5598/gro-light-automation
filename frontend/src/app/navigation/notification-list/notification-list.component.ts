4eb5b69716e9317ea4f9c0a01c0655c872fec8f6
Jason White <jmw5598@gmail.com>
2017-08-06 13:35:22 -0400

    initial commit for notification-list

---
+++ frontend/src/app/navigation/notification-list/notification-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrls: ['./notification-list.component.css']
+})
+export class NotificationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
