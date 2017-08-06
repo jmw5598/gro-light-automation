f511ac9ff70368d60cbaf721ddd36ce462293cb6
Jason White <jmw5598@gmail.com>
2017-08-06 10:02:07 -0400

    add method to get notifications by state (read/unread)

--- backend/src/main/java/com/gro/repository/NotificationRepository.java
+++ backend/src/main/java/com/gro/repository/NotificationRepository.java
@@ -1,10 +1,13 @@
 package com.gro.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gro.model.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
     
+    public Page<Notification> findAllByHasRead(Boolean read, Pageable pageable);
 
 }
