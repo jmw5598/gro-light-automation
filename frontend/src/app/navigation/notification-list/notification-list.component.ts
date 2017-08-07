fffb76e094d11926ec7132ec487345fecdb62e62
Jason White <jmw5598@gmail.com>
2017-08-07 12:31:50 -0400

    created notification-list component to display multiple notification components

--- frontend/src/app/navigation/notification-list/notification-list.component.ts
+++ frontend/src/app/navigation/notification-list/notification-list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+
+import { Notification } from '@app/shared/model/notification/notification.model';
 
 @Component({
-  selector: 'app-notification-list',
+  selector: 'gro-notification-list',
   templateUrl: './notification-list.component.html',
   styleUrls: ['./notification-list.component.css']
 })
 export class NotificationListComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  notifications: Array<Notification>;
+
+  @Output()
+  onUpdateState: EventEmitter<Notification> = new EventEmitter<Notification>();
+
+  @Output()
+  onDeleteNotification: EventEmitter<Notification> = new EventEmitter<Notification>();
+
+  constructor() {
+    this.notifications = new Array<Notification>();
+  }
 
   ngOnInit() {
+  }
+
+  updateState(notification: Notification) {
+    this.onUpdateState.emit(notification);
+  }
+
+  delete(notification: Notification) {
+    this.onDeleteNotification.emit(notification);
   }
 
 }
