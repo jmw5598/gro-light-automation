6d3ade6ad190f0ebb94972e0a3219323de052fa5
Jason White <jmw5598@gmail.com>
2017-08-07 12:32:12 -0400

    updatedn navigation module with new components

--- frontend/src/app/navigation/navigation.module.ts
+++ frontend/src/app/navigation/navigation.module.ts
@@ -5,12 +5,13 @@ import { SharedModule } from '@app/shared/shared.module';
 import { NavigationComponent } from './navigation.component';
 import { NotificationDropdownComponent } from './notification-dropdown/notification-dropdown.component';
 import { NotificationComponent } from './notification/notification.component';
+import { NotificationListComponent } from './notification-list/notification-list.component';
 
 @NgModule({
   imports: [
     SharedModule
   ],
   exports: [NavigationComponent],
-  declarations: [NavigationComponent, NotificationDropdownComponent, NotificationComponent]
+  declarations: [NavigationComponent, NotificationDropdownComponent, NotificationComponent, NotificationListComponent]
 })
 export class NavigationModule {}
