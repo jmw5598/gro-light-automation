f3674711cabfc233a86a0adaab745ece267bd4e6
Jason White <jmw5598@gmail.com>
2017-08-07 12:29:50 -0400

    updated notification dropdown to handle notification updates, deletes, and pulling in notification by state

--- frontend/src/app/navigation/notification-dropdown/notification-dropdown.component.ts
+++ frontend/src/app/navigation/notification-dropdown/notification-dropdown.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Notification } from '@app/shared/model/notification/notification.model';
+import { NotificationView } from '@app/shared/model/notification/notification-view.enum';
+import { NotificationService } from '@app/core/service/notification/notification.service';
+import { Page } from '@app/shared/model/paging/page.model';
 import { SseService } from '@app/core/service/sse/sse.service';
 
 @Component({
@@ -11,30 +14,66 @@ import { SseService } from '@app/core/service/sse/sse.service';
 export class NotificationDropdownComponent implements OnInit, OnDestroy {
 
   private subscription;
-  private notifications: Array<Notification>;
+  private read: Page<Notification>;
+  private unread: Page<Notification>;
+  private notificationView = NotificationView;
 
-  constructor(private sseService: SseService) {
-    this.notifications = new Array<Notification>();
-  }
+  private notificationViewSelected: NotificationView = NotificationView.UNREAD;
+
+  constructor(
+    private sseService: SseService,
+    private notificationService: NotificationService
+  ) { }
 
   ngOnInit() {
     this.subscription = this.sseService
       .notificationAlert
         .subscribe(
-          notification => this.handleNotification(notification)
+          notification => this.handleNotificationEvent(notification)
         );
+    this.loadNotificationsByState(true);  // load read notificaitons
+    this.loadNotificationsByState(false); // load unread notifications
+  }
+
+  loadNotificationsByState(state: boolean) {
+    this.notificationService.findAllByState(state)
+      .subscribe(
+        data => state ? this.read = data : this.unread = data,
+        error => console.log("error getting notifications" + error)
+      )
+  }
+
+  changeView(view: NotificationView) {
+    this.notificationViewSelected = view;
+  }
+
+  onUpdateState(notification: Notification) {
+    this.notificationService.updateNotificationState(notification.id, notification.isRead)
+      .subscribe(
+        data => {
+          this.loadNotificationsByState(true);
+          this.loadNotificationsByState(false);
+        },
+        error => console.log("error updating notification")
+      )
   }
 
   onDeleteNotification(notification: Notification) {
-    let index = this.notifications.indexOf(notification);
-    if(index > -1)
-      this.notifications.splice(index, 1);
+    this.notificationService.delete(notification.id)
+      .subscribe(
+        data => {
+          this.loadNotificationsByState(true);
+          this.loadNotificationsByState(false);
+        },
+        error => console.log(error)
+      );
   }
 
-  private handleNotification(object) {
+  private handleNotificationEvent(object) {
     let notification: Notification =
-        new Notification(object.id, new Date(object.timestamp), object.message, object.component);
-    this.notifications.unshift(notification);
+        new Notification(object.id, new Date(object.timestamp), object.message, object.component, object.isRead);
+    this.unread.content.unshift(notification);
+    this.unread.totalElements++;
   }
 
   ngOnDestroy() {
