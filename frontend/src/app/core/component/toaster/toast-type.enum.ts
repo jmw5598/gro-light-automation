e3249177083f9d7cec8887fe9d082f0c4ac7d4c4
Jason White <jmw5598@gmail.com>
2017-08-09 19:07:49 -0400

    created toast-type enum

---
+++ frontend/src/app/core/component/toaster/toast-type.enum.ts
@@ -0,0 +1,7 @@
+export enum ToastType {
+  DEFAULT = <any>"alert-default",
+  SUCCESS = <any>"alert-success",
+  INFO = <any>"alert-info",
+  WANRING = <any>"alert-warning",
+  DANGER = <any>"alert-danger"
+}
