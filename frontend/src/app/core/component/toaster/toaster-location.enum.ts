0a49f7a0c120fd9c691607bc409cadf96fa88cdc
Jason White <jmw5598@gmail.com>
2017-08-10 15:11:48 -0400

    create toaster location enum

---
+++ frontend/src/app/core/component/toaster/toaster-location.enum.ts
@@ -0,0 +1,6 @@
+export enum ToasterLocation {
+  TOP_LEFT = <any>"toaster-location-top-left",
+  TOP_RIGHT = <any>"toaster-location-top-right",
+  BOTTOM_LEFT = <any>"toaster-location-bottom-left",
+  BOTTOM_RIGHT = <any>"toaster-location-bottom-right"
+}
