6b39ac339b3ab60398ddb56e10e2e002bf70047b
Jason White <jmw5598@gmail.com>
2017-08-11 13:40:53 -0400

    add toaster component to app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { ToasterLocation } from '@app/core/component/toaster/toaster-location.enum';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  private toasterLocation = ToasterLocation;
 }
