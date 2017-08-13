f5cc32e0b81980aca3597c7f586a033f090f4fa9
Jason White <jmw5598@gmail.com>
2017-08-13 17:28:05 -0400

    created moisture message transformer

---
+++ backend/src/main/java/com/gro/messaging/transformer/MoistureMessageTransformer.java
@@ -0,0 +1,26 @@
+package com.gro.messaging.transformer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.Transformer;
+import org.springframework.integration.support.json.Jackson2JsonObjectMapper;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+import com.gro.model.moisture.MoistureData;
+
+@MessageEndpoint
+public class MoistureMessageTransformer {
+
+    @Autowired
+    private Jackson2JsonObjectMapper jackson2JsonObjectMapper;
+    
+    @Transformer(inputChannel="moistureTransformerChannel",
+            outputChannel="moistureServiceChannel")
+    public Message<MoistureData> transform(Message<String> message) throws Exception {
+        String payload = message.getPayload();
+        MoistureData data = jackson2JsonObjectMapper.fromJson(payload, MoistureData.class);
+        return MessageBuilder.createMessage(data, message.getHeaders());        
+    }
+    
+}
