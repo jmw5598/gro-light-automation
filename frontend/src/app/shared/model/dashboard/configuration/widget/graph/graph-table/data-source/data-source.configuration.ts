d7d8e79e413614faad0e1aeb31e55d1e18276f6c
Jason White <jmw5598@gmail.com>
2017-08-23 14:39:35 -0400

    added component attribute to datasource configuration model

--- frontend/src/app/shared/model/dashboard/configuration/widget/graph/graph-table/data-source/data-source.configuration.ts
+++ frontend/src/app/shared/model/dashboard/configuration/widget/graph/graph-table/data-source/data-source.configuration.ts
@@ -1,12 +1,14 @@
 import { MetricCalculation } from './metric-calculation.enum';
 import { MetricDataType } from './metric-data-type.enum';
 import { MetricTimeSpan } from './metric-time-span.enum';
+import { RPiComponent } from '@app/shared/model/rpicomponent/rpicomponent.model';
 
 export class DataSourceConfiguration {
 
   public calculation: MetricCalculation;
   public dataType: MetricDataType;
   public timeSpan: MetricTimeSpan;
+  public component: RPiComponent;
 
   constructor() {
     this.calculation = MetricCalculation.AVERAGE;
