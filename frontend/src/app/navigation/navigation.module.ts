0b5b39249aba10144f5edbd75eb65ddb7f59021e
Jason White <jmw5598@gmail.com>
2017-08-28 19:25:59 -0400

    added declaration for notification infinite scroll

--- frontend/src/app/navigation/navigation.module.ts
+++ frontend/src/app/navigation/navigation.module.ts
@@ -6,12 +6,13 @@ import { NavigationComponent } from './navigation.component';
 import { NotificationDropdownComponent } from './notification-dropdown/notification-dropdown.component';
 import { NotificationComponent } from './notification/notification.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
+import { NotificationInfiniteScrollDirective } from './notification-list/notification-infinite-scroll.directive';
 
 @NgModule({
   imports: [
     SharedModule
   ],
   exports: [NavigationComponent],
-  declarations: [NavigationComponent, NotificationDropdownComponent, NotificationComponent, NotificationListComponent]
+  declarations: [NavigationComponent, NotificationDropdownComponent, NotificationComponent, NotificationListComponent, NotificationInfiniteScrollDirective]
 })
 export class NavigationModule {}
