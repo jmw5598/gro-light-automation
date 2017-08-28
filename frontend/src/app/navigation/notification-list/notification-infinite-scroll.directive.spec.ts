4529cd1fc1fd1a954e3563bf85b5054bf46890c0
Jason White <jmw5598@gmail.com>
2017-08-28 19:24:20 -0400

    created notifications infinite scroll directive

---
+++ frontend/src/app/navigation/notification-list/notification-infinite-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NotificationInfiniteScrollDirective } from './notification-infinite-scroll.directive';
+
+describe('NotificationInfiniteScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NotificationInfiniteScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/navigation/notification-list/notification-infinite-scroll.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appNotificationInfiniteScroll]'
+})
+export class NotificationInfiniteScrollDirective {
+
+  constructor() { }
+
+}
