42841a6853cb7e4050b83db738769c3ea3de007a
Jason White <jmw5598@gmail.com>
2017-11-10 14:17:51 -0500

    created skeleton scheduling configuration class

---
+++ backend/src/main/java/com/gro/scheduling/SchedulingConfig.java
@@ -0,0 +1,11 @@
+package com.gro.scheduling;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SchedulingConfig {
+    
+    // Setup Quartz scheduler
+    // Pull in scheduled jobs from db and start the ones needed.??
+
+}
