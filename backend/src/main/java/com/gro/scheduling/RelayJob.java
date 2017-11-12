b4b76cd554bda199a493990ab621b72e159bfdc3
Jason White <jmw5598@gmail.com>
2017-11-12 10:50:02 -0500

    created logic for RelayJob class

--- backend/src/main/java/com/gro/scheduling/RelayJob.java
+++ backend/src/main/java/com/gro/scheduling/RelayJob.java
@@ -3,10 +3,13 @@ package com.gro.scheduling;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.gro.model.relay.RelayDTO;
+import com.gro.model.relay.RelayScheduleJob;
 import com.gro.model.relay.RelayState;
 import com.gro.model.rpicomponent.RPiComponent;
 import com.gro.web.service.RelayService;
@@ -16,18 +19,21 @@ public class RelayJob implements Job {
     
     @Autowired
     private RelayService relayService;
+    
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-//        RPiComponent component = (RPiComponent) context.getJobDetail().getJobDataMap().get("component");
-//        RelayState state = (RelayState) context.getJobDetail().getJobDataMap().get("state");
-//        RelayDTO relay = new RelayDTO(component, state);
-//        try {
-//            relayService.toggle(relay);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        System.out.println("\n\nSending MQTT message to toggle relay\n\n");
+        RelayScheduleJob job = (RelayScheduleJob) context.getJobDetail().getJobDataMap().get("schedule");
+        RelayDTO relayDto = new RelayDTO(job.getComponent(), job.getState());
+        try {
+            relayService.toggle(relayDto);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        System.out.println("\n\nSending MQTT message to toggle relay");
+        System.out.println(context.getJobDetail().getJobDataMap().get("schedule"));
+        
     }
 
 }
