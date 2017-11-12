eda9ac28b8b3a876467e26d60ff8fddbd204e99e
Jason White <jmw5598@gmail.com>
2017-11-12 10:40:22 -0500

    created RelayScheduleJob entity

---
+++ backend/src/main/java/com/gro/model/relay/RelayScheduleJob.java
@@ -0,0 +1,82 @@
+package com.gro.model.relay;
+
+import java.sql.Time;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import com.gro.model.rpicomponent.RPiComponent;
+
+@Entity
+public class RelayScheduleJob {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    
+    @NotNull
+    @OneToOne
+    private RPiComponent component;
+    
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RelayState state;
+    
+    @NotNull
+    private Boolean enabled;
+    
+    @NotNull
+    private Time time;
+    
+    public RelayScheduleJob() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public RPiComponent getComponent() {
+        return component;
+    }
+
+    public void setComponent(RPiComponent component) {
+        this.component = component;
+    }
+
+    public RelayState getState() {
+        return state;
+    }
+
+    public void setState(RelayState state) {
+        this.state = state;
+    }
+
+    public Boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+    
+    public Time getTime() {
+        return this.time;
+    }
+    
+    public void setTime(Time time) {
+        this.time = time;
+    }
+    
+    
+    
+}
