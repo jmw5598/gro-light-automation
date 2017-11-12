97742df4653f9c67f9dfb2ce9606a3408bebe5d6
Jason White <jmw5598@gmail.com>
2017-11-12 10:40:45 -0500

    created repository to store schedules for relays

---
+++ backend/src/main/java/com/gro/repository/RelayScheduleJobRepository.java
@@ -0,0 +1,14 @@
+package com.gro.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.gro.model.relay.RelayScheduleJob;
+
+@PreAuthorize("hasRole('ADMIN')")
+public interface RelayScheduleJobRepository extends CrudRepository<RelayScheduleJob, Integer> {
+    List<RelayScheduleJob> findByEnabled(Boolean enabled);
+    List<RelayScheduleJob> findAllByComponentId(Integer id);
+}
