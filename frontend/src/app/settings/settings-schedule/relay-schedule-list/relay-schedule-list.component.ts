73d85c2bdaf1e276ab707bddace116d842ed04f1
Jason White <jmw5598@gmail.com>
2017-11-14 17:04:09 -0500

    created relay schedule list component

--- frontend/src/app/settings/settings-schedule/relay-schedule-list/relay-schedule-list.component.ts
+++ frontend/src/app/settings/settings-schedule/relay-schedule-list/relay-schedule-list.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { RelayScheduleJob } from '@app/shared/model/rpicomponent/relay-schedule-job.model';
 
 @Component({
-  selector: 'app-relay-schedule-list',
+  selector: 'gro-relay-schedule-list',
   templateUrl: './relay-schedule-list.component.html',
   styleUrls: ['./relay-schedule-list.component.css']
 })
 export class RelayScheduleListComponent implements OnInit {
+  scheduleType: string = "FIXED";
+  @Input()
+  schedules: Array<RelayScheduleJob>;
+
+  @Output()
+  deleteSchedule: EventEmitter<number> = new EventEmitter<number>();
+
+  @Output()
+  editSchedule: EventEmitter<number> = new EventEmitter<number>();
+
+  @Output()
+  toggleSchedule: EventEmitter<RelayScheduleJob> = new EventEmitter<RelayScheduleJob>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  delete(id: number) {
+    this.deleteSchedule.emit(id);
+  }
+
+  edit(id: number) {
+    this.editSchedule.emit(id);
+  }
+
+  toggleEnabled(schedule: RelayScheduleJob) {
+    this.toggleSchedule.emit(schedule);
   }
 
 }
