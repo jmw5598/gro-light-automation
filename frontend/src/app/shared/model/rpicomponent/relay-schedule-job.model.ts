77226d0cfd31aa9f0b35d0661d5240172ac9dfd9
Jason White <jmw5598@gmail.com>
2017-11-14 17:06:06 -0500

    created relay schedule job model

---
+++ frontend/src/app/shared/model/rpicomponent/relay-schedule-job.model.ts
@@ -0,0 +1,12 @@
+import { RPiComponent } from './rpicomponent.model';
+import { RelayState } from './relay-state.enum';
+
+export class RelayScheduleJob {
+  constructor(
+      public id: number,
+      public component: RPiComponent,
+      public state: RelayState,
+      public enabled: boolean,
+      public time: Date
+  ) {}
+}
