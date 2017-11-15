8635f5924bad3c4b1f7e3aebf4091615b1d92822
Jason White <jmw5598@gmail.com>
2017-11-15 18:13:17 -0500

    fixed form reset issue

--- frontend/src/app/settings/rpicomponent-add/rpicomponent-add.component.ts
+++ frontend/src/app/settings/rpicomponent-add/rpicomponent-add.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 
 import { RPiComponent } from '@app/shared/model/rpicomponent/rpicomponent.model';
 import { RPiComponentService } from '@app/core/service/rpicomponent/rpicomponent.service';
-
+import { RPiComponentFormComponent } from '@app/shared/component/rpicomponent-form/rpicomponent-form.component';
+import { ToasterService } from '@app/core/component/toaster/toaster.service';
+import { ToastType } from '@app/core/component/toaster/toast-type.enum';
 
 @Component({
   selector: 'gro-rpicomponent-add',
@@ -11,12 +13,17 @@ import { RPiComponentService } from '@app/core/service/rpicomponent/rpicomponent
 })
 export class RPiComponentAddComponent implements OnInit {
 
+  @ViewChild(RPiComponentFormComponent) child: RPiComponentFormComponent;
+
   formSettings : Object = {
   	submitLabel : 'Create',
   	isClearable : true
   };
 
-  constructor(private rPiComponentService: RPiComponentService) { }
+  constructor(
+    private rPiComponentService: RPiComponentService,
+    private toasterService: ToasterService
+  ) { }
 
   ngOnInit() {
   }
@@ -25,9 +32,16 @@ export class RPiComponentAddComponent implements OnInit {
     console.log(component);
   	this.rPiComponentService.save(component)
       .subscribe(
-        data => { console.log(data); },
-        error => console.log("error saving component")
+        data => {
+          this.resetForm();
+          this.toasterService.toast("Component successfully added", ToastType.SUCCESS);
+        },
+        error => this.toasterService.toast("Error adding new component", ToastType.WARNING)
       );
+  }
+
+  private resetForm() {
+    this.child.reset();
   }
 
 }
