b0de1f43ab16e6739cc96d6597aca0396a2668bc
Jason White <jmw5598@gmail.com>
2017-11-16 09:24:52 -0500

    skeleton component for confirmation modal window

---
+++ frontend/src/app/shared/component/modal-confirm/modal-confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-confirm',
+  templateUrl: './modal-confirm.component.html',
+  styleUrls: ['./modal-confirm.component.css']
+})
+export class ModalConfirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
