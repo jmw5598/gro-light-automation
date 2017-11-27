63a8e8c926ad88081b95d6fa03bb945186d5fad0
Jason White <jmw5598@gmail.com>
2017-11-27 09:33:58 -0500

    refactored *ScheduleJob model classes

---
+++ backend/src/main/java/com/gro/model/AbstractScheduleJob.java
@@ -0,0 +1,50 @@
+package com.gro.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import com.gro.model.rpicomponent.RPiComponent;
+
+@MappedSuperclass
+public class AbstractScheduleJob {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+    
+    @OneToOne
+    @NotNull
+    private RPiComponent component;
+    
+    @NotNull
+    private Boolean enabled;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public RPiComponent getComponent() {
+        return component;
+    }
+
+    public void setComponent(RPiComponent component) {
+        this.component = component;
+    }
+    
+    public Boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+    
+}
--- backend/src/main/java/com/gro/model/relay/RelayScheduleJob.java
+++ backend/src/main/java/com/gro/model/relay/RelayScheduleJob.java
@@ -5,33 +5,18 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 
-import com.gro.model.rpicomponent.RPiComponent;
+import com.gro.model.AbstractScheduleJob;
 
 @Entity
-public class RelayScheduleJob {
-    
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Integer id;
-    
-    @NotNull
-    @OneToOne
-    private RPiComponent component;
+public class RelayScheduleJob extends AbstractScheduleJob {
     
     @NotNull
     @Enumerated(EnumType.STRING)
     private RelayState state;
-    
-    @NotNull
-    private Boolean enabled;
     
     @NotNull
     @Temporal(TemporalType.TIMESTAMP)
@@ -39,36 +24,12 @@ public class RelayScheduleJob {
     
     public RelayScheduleJob() {}
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public RPiComponent getComponent() {
-        return component;
-    }
-
-    public void setComponent(RPiComponent component) {
-        this.component = component;
-    }
-
     public RelayState getState() {
         return state;
     }
 
     public void setState(RelayState state) {
         this.state = state;
-    }
-
-    public Boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
     }
     
     public Date getTime() {
