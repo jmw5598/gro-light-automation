beb1603bcf7da14d246776853763971339e84a9d
Jason White <jmw5598@gmail.com>
2017-12-04 09:38:56 -0500

    updated rpi-pinout component and template

--- frontend/src/app/settings/settings-rpicomponent/rpi-pinout/rpi-pinout.component.ts
+++ frontend/src/app/settings/settings-rpicomponent/rpi-pinout/rpi-pinout.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { RPiComponent } from '@app/shared/model/rpicomponent/rpicomponent.model';
+import { RPiPin } from '@app/shared/model/rpipin/rpi-pin.model';
 
 @Component({
-  selector: 'app-rpi-pinout',
+  selector: 'gro-rpi-pinout',
   templateUrl: './rpi-pinout.component.html',
   styleUrls: ['./rpi-pinout.component.css']
 })
-export class RpiPinoutComponent implements OnInit {
+export class RPiPinoutComponent implements OnInit {
+
+  @Input()
+  selected: RPiComponent;
+
+  @Input()
+  pins: Array<RPiPin>;
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
