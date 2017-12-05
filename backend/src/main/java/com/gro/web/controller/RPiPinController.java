fcfa5ca3a28c983e3999e8ebdfd0f1fc5ad9d6c0
Jason White <jmw5598@gmail.com>
2017-12-05 11:15:01 -0500

    added getPinById endpoint

--- backend/src/main/java/com/gro/web/controller/RPiPinController.java
+++ backend/src/main/java/com/gro/web/controller/RPiPinController.java
@@ -3,11 +3,14 @@ package com.gro.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gro.model.rpicomponent.RPiPin;
+import com.gro.model.rpicomponent.RPiPinNotFoundException;
 import com.gro.repository.RPiPinRepository;
 
 @RestController
@@ -17,9 +20,25 @@ public class RPiPinController {
     @Autowired
     private RPiPinRepository rPiPinRepository;
     
+    @Value("${exception.pin-not-found}")
+    private String pinNotFound;
+    
     @RequestMapping(method=RequestMethod.GET)
     public List<RPiPin> getAllPins() {
         return rPiPinRepository.findAll();
+    }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public RPiPin getPinById(@PathVariable("id") Integer id) {
+        return validateRPiPin(id);
+    }
+    
+    private RPiPin validateRPiPin(Integer id) {
+        RPiPin pin = rPiPinRepository.findOne(id);
+        if(pin == null)
+            throw new RPiPinNotFoundException(pinNotFound);
+        else
+            return pin;
     }
 
 }
