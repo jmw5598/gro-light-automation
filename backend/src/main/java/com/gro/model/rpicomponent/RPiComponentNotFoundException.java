2b13ac8161ba0415223c4af5f7c332c388e7a32a
Jason White <jmw5598@gmail.com>
2017-12-05 11:09:43 -0500

    made exception extend NotFoundException

--- backend/src/main/java/com/gro/model/rpicomponent/RPiComponentNotFoundException.java
+++ backend/src/main/java/com/gro/model/rpicomponent/RPiComponentNotFoundException.java
@@ -1,6 +1,10 @@
 package com.gro.model.rpicomponent;
 
-public class RPiComponentNotFoundException extends RuntimeException {
+import com.gro.model.NotFoundException;
+
+public class RPiComponentNotFoundException extends NotFoundException {
+
+    private static final long serialVersionUID = 5009555411974113907L;
 
     public RPiComponentNotFoundException(String message) {
         super(message);
