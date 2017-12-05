6a3dec21b75760ab26ba60a4c82d91432ebe69c4
Jason White <jmw5598@gmail.com>
2017-12-05 11:09:25 -0500

    made exception extend NotFoundException

--- backend/src/main/java/com/gro/model/notification/NotificationNotFoundException.java
+++ backend/src/main/java/com/gro/model/notification/NotificationNotFoundException.java
@@ -1,7 +1,11 @@
 package com.gro.model.notification;
 
-public class NotificationNotFoundException extends RuntimeException {
-    
+import com.gro.model.NotFoundException;
+
+public class NotificationNotFoundException extends NotFoundException {
+
+    private static final long serialVersionUID = -8762503873478598927L;
+
     public NotificationNotFoundException(String message) {
         super(message);
     }
