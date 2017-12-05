2b7139eb092e4c6026df43b06b915e06465d32be
Jason White <jmw5598@gmail.com>
2017-12-05 11:14:11 -0500

    added PreAuthorize to delete and save methods

--- backend/src/main/java/com/gro/repository/RPiPinRepository.java
+++ backend/src/main/java/com/gro/repository/RPiPinRepository.java
@@ -3,6 +3,7 @@ package com.gro.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.gro.model.rpicomponent.RPiPin;
@@ -11,5 +12,32 @@ import com.gro.model.rpicomponent.RPiPin;
 public interface RPiPinRepository extends CrudRepository<RPiPin, Integer> {
     
     List<RPiPin> findAll();
+
+    @Override
+    default <S extends RPiPin> S save(S entity) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    <S extends RPiPin> Iterable<S> save(Iterable<S> entities);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(Integer id);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(RPiPin entity);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void delete(Iterable<? extends RPiPin> entities);
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @Override
+    void deleteAll();
+    
     
 }
