9a4c923759e6c6006fdbaf98a2d16bb5d9227a8f
Jason White <jmw5598@gmail.com>
2017-12-18 10:59:37 -0500

    created data.sql to create sequence_values, function to generate next sequence value, and triggers to serve  unique id's to component tables on insert

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,104 @@
+----------------------------------------------------------------------
+-- TABLE & FUNCTION ARE DROP/CREATED WHEN
+-- PROPERTY jpa.hibernate.ddl-auto = create
+-- THIS SHOULD BE IN schema.sql BUT data.sql WILL NOT RUN IF
+-- THE schema.sql IS ON THE CLASS PATH
+----------------------------------------------------------------------
+-- DROP/CREATES sequence_values TABLE
+DROP TABLE IF EXISTS sequence_values ^;
+CREATE TABLE sequence_values (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  thread_id INT NOT NULL,
+  created DATETIME DEFAULT CURRENT_TIMESTAMP
+) ^;
+
+-- CREATES FUNCTION THAT GENERATES THE ID'S 
+-- IN sequence_values TABLE
+DROP FUNCTION IF EXISTS sequence_nextval ^;
+CREATE FUNCTION sequence_nextval()
+RETURNS INTEGER
+NOT DETERMINISTIC
+MODIFIES SQL DATA
+BEGIN
+  DECLARE nextval INTEGER;
+  INSERT INTO sequence_values (thread_id) VALUE (CONNECTION_ID());
+  SELECT id FROM sequence_values ORDER BY created DESC LIMIT 1 INTO nextval;
+  RETURN nextval;
+END ^;
+
+----------------------------------------------------------------------
+-- TRIGGERS ARE ONLY DROP/CREATED WHEN 
+----------------------------------------------------------------------
+-- TEMPERATURE SENSOR TRIGGER
+DROP TRIGGER IF EXISTS temperature_sensor_id_trigger ^;
+CREATE TRIGGER temperature_sensor_id_trigger
+BEFORE INSERT
+  ON temperature_sensor FOR EACH ROW
+BEGIN
+  SET NEW.id = sequence_nextval();
+END ^;
+
+-- HUMIDITY SENSOR TRIGGER
+DROP TRIGGER IF EXISTS humidity_sensor_id_trigger ^;
+CREATE TRIGGER humidity_sensor_id_trigger
+BEFORE INSERT
+  ON humidity_sensor FOR EACH ROW
+BEGIN
+  SET NEW.id = sequence_nextval();
+END ^;
+
+-- TEMPERATURE_HUMIDITY SENSOR TRIGGER
+DROP TRIGGER IF EXISTS temperature_humidity_sensor_id_trigger ^;
+CREATE TRIGGER temperature_humidity_sensor_id_trigger
+BEFORE INSERT
+  ON temperature_humidity_sensor FOR EACH ROW
+BEGIN
+  SET NEW.id = sequence_nextval();
+END ^;
+
+-- PROXIMITY SENSOR TRIGGER
+DROP TRIGGER IF EXISTS proximity_sensor_id_trigger ^;
+CREATE TRIGGER proximity_sensor_id_trigger
+BEFORE INSERT
+  ON proximity_sensor FOR EACH ROW
+BEGIN
+  SET NEW.id = sequence_nextval();
+END ^;
+
+-- MOISTURE SENSOR TRIGGER
+DROP TRIGGER IF EXISTS moisture_sensor_id_trigger ^;
+CREATE TRIGGER moisture_sensor_id_trigger
+BEFORE INSERT
+  ON moisture_sensor FOR EACH ROW
+BEGIN
+  SET NEW.id = sequence_nextval();
+END ^;
+
+-- RELAY TRIGGER
+DROP TRIGGER IF EXISTS relay_id_trigger ^;
+CREATE TRIGGER relay_id_trigger
+BEFORE INSERT
+  ON relay FOR EACH ROW
+BEGIN
+  SET NEW.id = sequence_nextval();
+END ^;
+
+----------------------------------------------------------------------
+-- INSERT USERS FOR TESTING
+----------------------------------------------------------------------
+INSERT INTO user (id, username, password, firstname, lastname, email, enabled, last_password_reset_date) 
+VALUES (1, 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 'admin', 'admin@admin.com', 1, NOW()) ^;
+
+INSERT INTO user (id, username, password, firstname, lastname, email, enabled, last_password_reset_date) 
+VALUES (2, 'user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 'user', 'enabled@user.com', 1, NOW()) ^;
+
+INSERT INTO user (id, username, password, firstname, lastname, email, enabled, last_password_reset_date) 
+VALUES (3, 'disabled', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 'user', 'disabled@user.com', 0, NOW()) ^;
+
+INSERT INTO authority (id, name) VALUES (1, 'ROLE_USER') ^;
+INSERT INTO authority (id, name) VALUES (2, 'ROLE_ADMIN') ^;
+
+INSERT INTO user_authority (user_id, authority_id) VALUES (1, 1) ^;
+INSERT INTO user_authority (user_id, authority_id) VALUES (1, 2) ^;
+INSERT INTO user_authority (user_id, authority_id) VALUES (2, 1) ^;
+INSERT INTO user_authority (user_id, authority_id) VALUES (3, 1) ^;
