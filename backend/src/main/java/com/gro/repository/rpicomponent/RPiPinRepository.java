c7295480bd0e98a026f7ad812e7c943865f6fa4c
Jason White <jmw5598@gmail.com>
2017-12-23 08:52:27 -0500

    refactored rpi pin repository to extend ReadOnlyRepository

--- backend/src/main/java/com/gro/repository/rpicomponent/RPiPinRepository.java
+++ backend/src/main/java/com/gro/repository/rpicomponent/RPiPinRepository.java
@@ -1,43 +1,11 @@
 package com.gro.repository.rpicomponent;
 
-import java.util.List;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.gro.model.rpipin.RPiPin;
+import com.gro.repository.ReadOnlyRepository;
 
 @Repository
-public interface RPiPinRepository extends CrudRepository<RPiPin, Integer> {
-    
-    List<RPiPin> findAll();
-
-    @Override
-    default <S extends RPiPin> S save(S entity) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @Override
-    <S extends RPiPin> Iterable<S> save(Iterable<S> entities);
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @Override
-    void delete(Integer id);
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @Override
-    void delete(RPiPin entity);
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @Override
-    void delete(Iterable<? extends RPiPin> entities);
-
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @Override
-    void deleteAll();
-    
+public interface RPiPinRepository extends ReadOnlyRepository<RPiPin, Integer> {
     
 }
