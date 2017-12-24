cd1f40a638d9170e1e7986d63201c1af24d0fa2b
Jason White <jmw5598@gmail.com>
2017-12-24 10:22:24 -0500

    created sensor services

--- frontend/src/app/core/service/temperature-humidity-sensor/temperature-humidity-sensor.service.ts
+++ frontend/src/app/core/service/temperature-humidity-sensor/temperature-humidity-sensor.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { CrudService } from '@app/core/service/crud.service';
+import { REQUEST_OPTIONS_DEFAULT } from '@app/core/service/request-options.default';
+import { BASE_API_URL } from '@app/core/service/base-api-url.default';
 
 @Injectable()
-export class TemperatureHumiditySensorService {
+export class TemperatureHumiditySensorService extends CrudService<any, number> {
 
-  constructor() { }
+  constructor(http: Http) {
+    super(BASE_API_URL + "component/temperature-humidity", http, REQUEST_OPTIONS_DEFAULT)
+  }
 
 }
