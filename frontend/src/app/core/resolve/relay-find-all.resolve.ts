4fbb381518ca0de34ceb7474e6e3cb35a56929d5
Jason White <jmw5598@gmail.com>
2017-12-25 11:49:21 -0500

    added Injectable decorator to relay resolves

--- frontend/src/app/core/resolve/relay-find-all.resolve.ts
+++ frontend/src/app/core/resolve/relay-find-all.resolve.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@angular/core';
 import { FindAllResolve } from './find-all.resolve';
 import { RelayService } from '../service/relay/relay.service';
 
+@Injectable()
 export class RelayFindAllResolve extends FindAllResolve<any> {
 
   constructor(protected service: RelayService) {
--- frontend/src/app/core/resolve/relay-find-one.resolve.ts
+++ frontend/src/app/core/resolve/relay-find-one.resolve.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@angular/core';
 import { FindOneResolve } from './find-one.resolve';
 import { RelayService } from '../service/relay/relay.service';
 
+@Injectable()
 export class RelayFindOneResolve extends FindOneResolve<any> {
 
   constructor(protected service: RelayService) {
