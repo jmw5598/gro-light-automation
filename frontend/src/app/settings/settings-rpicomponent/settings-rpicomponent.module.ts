9a27474c813dd3bc3226ddb69c6c592764d181ea
Jason White <jmw5598@gmail.com>
2017-12-29 10:49:40 -0500

    created settings-rpicomponent.module.ts and cleaned up settings.module.ts

---
+++ frontend/src/app/settings/settings-rpicomponent/settings-rpicomponent.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '@app/shared/shared.module';
+
+import { SettingsRPiComponentComponent } from './settings-rpicomponent.component';
+import { RPiComponentAddComponent } from './rpicomponent-add/rpicomponent-add.component';
+import { RPiComponentEditComponent } from './rpicomponent-edit/rpicomponent-edit.component';
+import { RPiComponentFilterComponent } from './rpicomponent-table/rpicomponent-filter/rpicomponent-filter.component';
+import { RPiComponentFormComponent } from './rpicomponent-form/rpicomponent-form.component';
+import { RPiComponentListComponent } from './rpicomponent-table/rpicomponent-list/rpicomponent-list.component';
+import { RPiComponentTableComponent } from './rpicomponent-table/rpicomponent-table.component';
+import { RPiPinoutComponent } from './rpi-pinout/rpi-pinout.component';
+
+@NgModule({
+  imports : [
+    SharedModule
+  ],
+  exports: [
+    SettingsRPiComponentComponent
+  ],
+  declarations: [
+    RPiComponentAddComponent,
+    RPiComponentEditComponent,
+    RPiComponentFilterComponent,
+    RPiComponentFormComponent,
+    RPiComponentListComponent,
+    RPiComponentTableComponent,
+    RPiPinoutComponent,
+    SettingsRPiComponentComponent
+  ]
+})
+export class SettingsRPiComponentModule {}
--- frontend/src/app/settings/settings.module.ts
+++ frontend/src/app/settings/settings.module.ts
@@ -2,45 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UiSwitchModule } from 'angular2-ui-switch';
 
+import { SettingsRPiComponentModule } from './settings-rpicomponent/settings-rpicomponent.module';
+
 import { SETTINGS_ROUTES } from '@app/settings/settings.routing';
 
 import { CoreModule } from '@app/core/core.module';
 import { SharedModule } from '@app/shared/shared.module';
 
-import { RPiComponentAddComponent } from './settings-rpicomponent/rpicomponent-add/rpicomponent-add.component';
-import { RPiComponentEditComponent } from './settings-rpicomponent/rpicomponent-edit/rpicomponent-edit.component';
-import { RPiComponentsComponent } from './settings-rpicomponent/rpicomponents/rpicomponents.component';
-import { RPiComponentFormComponent } from './settings-rpicomponent/rpicomponent-form/rpicomponent-form.component';
 import { SettingsComponent } from './settings.component';
 import { SettingsNavigationComponent } from './settings-navigation/settings-navigation.component';
-import { SettingsRPiComponentComponent } from './settings-rpicomponent/settings-rpicomponent.component';
 import { SettingsScheduleComponent } from './settings-schedule/settings-schedule.component';
 import { SettingsUserComponent } from './settings-user/settings-user.component';
 import { RelayScheduleFormComponent } from './settings-schedule/relay-schedule-form/relay-schedule-form.component';
 import { RelayScheduleListComponent } from './settings-schedule/relay-schedule-list/relay-schedule-list.component';
-import { RPiPinoutComponent } from './settings-rpicomponent/rpi-pinout/rpi-pinout.component';
 
 
 @NgModule({
   imports: [
     SharedModule,
     UiSwitchModule,
+    SettingsRPiComponentModule,
     RouterModule.forChild(SETTINGS_ROUTES)
   ],
   exports: [SettingsComponent],
   declarations: [
-    RPiComponentAddComponent,
-    RPiComponentEditComponent,
-    RPiComponentFormComponent,
-    RPiComponentsComponent,
     SettingsComponent,
     SettingsNavigationComponent,
-    SettingsRPiComponentComponent,
     SettingsScheduleComponent,
     SettingsUserComponent,
     RelayScheduleFormComponent,
     RelayScheduleListComponent,
-    RPiPinoutComponent
   ]
 })
 export class SettingsModule {}
