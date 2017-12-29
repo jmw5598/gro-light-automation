2ce404e481ed2dd9845ba2762f99217cd5d58752
Jason White <jmw5598@gmail.com>
2017-12-29 10:28:32 -0500

    renamed rpicomponents component to rpicomponent-table

--- frontend/src/app/settings/settings-rpicomponent/rpicomponents/rpicomponent-filter/filter-by.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum FilterBy {
-  ALL = <any>"all",
-  HUMIDITY = <any>"humidity",
-  MOISTURE = <any>"moisture",
-  PROXIMITY = <any>"proximity",
-  RELAY = <any>"relay",
-  TEMPERATURE = <any>"temperature",
-  TEMPERATURE_HUMIDITY = <any>"temperature_humidity"
-}
