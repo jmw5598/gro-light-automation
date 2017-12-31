adb845895e717f12662811580b1910c4b68ba671
Jason White <jmw5598@gmail.com>
2017-12-31 08:33:57 -0500

    injected SettingsRPiComponentService and subscribed to selected component

--- frontend/src/app/settings/settings-rpicomponent/rpi-pinout/rpi-pinout.component.ts
+++ frontend/src/app/settings/settings-rpicomponent/rpi-pinout/rpi-pinout.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { RPiComponent } from '@app/shared/model/rpicomponent/rpicomponent.model';
 import { RPiPin } from '@app/shared/model/rpipin/rpi-pin.model';
+
+import { SettingsRPiComponentService } from '../settings-rpicomponent.service';
 
 @Component({
   selector: 'gro-rpi-pinout',
   templateUrl: './rpi-pinout.component.html',
   styleUrls: ['./rpi-pinout.component.css']
 })
-export class RPiPinoutComponent implements OnInit {
+export class RPiPinoutComponent implements OnInit, OnDestroy {
 
-  @Input()
   selected: RPiComponent;
+  private selectedSubscription;
 
   @Input()
   pins: Array<RPiPin>;
 
-  constructor() { }
+  constructor(
+    private settingsRPiComponentService: SettingsRPiComponentService
+  ) { }
 
   ngOnInit() {
+    this.selectedSubscription = this.settingsRPiComponentService.selectedComponent
+      .subscribe(
+        data => this.selected = data,
+        error => console.log("error subscribing to selected component")
+      );
+  }
 
+  ngOnDestroy() {
+    this.selectedSubscription.unsubscribe();
   }
 
 }
