95ce94f80dde1059d3b223ab7f42d0fb50d9d807
Jason White <jmw5598@gmail.com>
2018-01-02 09:48:41 -0500

    changed extension class

--- backend/src/main/java/com/gro/repository/rpicomponent/ProximitySensorRepository.java
+++ backend/src/main/java/com/gro/repository/rpicomponent/ProximitySensorRepository.java
@@ -1,11 +1,11 @@
 package com.gro.repository.rpicomponent;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gro.model.rpicomponent.component.ProximitySensor;
+import com.gro.repository.RestrictedWriteRepository;
 
 @Repository
-public interface ProximitySensorRepository extends CrudRepository<ProximitySensor, Integer> {
-    // TODO: Secure delete methods with with ADMIN role
+public interface ProximitySensorRepository extends RestrictedWriteRepository<ProximitySensor, Integer> {
+    
 }
