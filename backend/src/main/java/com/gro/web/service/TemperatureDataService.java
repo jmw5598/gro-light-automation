6c5126a3e021222610e2d608b14fe93907d4167b
Jason White <jmw5598@gmail.com>
2018-01-02 09:46:48 -0500

    updated imports

--- backend/src/main/java/com/gro/web/service/TemperatureDataService.java
+++ backend/src/main/java/com/gro/web/service/TemperatureDataService.java
@@ -2,7 +2,7 @@ package com.gro.web.service;
 
 import org.springframework.stereotype.Service;
 
-import com.gro.model.data.temperature.TemperatureDTO;
+import com.gro.model.rpicomponent.data.TemperatureDTO;
 
 @Service
 public class TemperatureDataService {
