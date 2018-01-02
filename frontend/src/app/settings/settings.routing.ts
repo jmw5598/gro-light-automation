dc84cf1aa63ef911d25334dbcd6367466b22c3bf
Jason White <jmw5598@gmail.com>
2018-01-02 10:02:21 -0500

    updated settings routing file

--- frontend/src/app/settings/settings.routing.ts
+++ frontend/src/app/settings/settings.routing.ts
@@ -7,6 +7,12 @@ import { SettingsUserComponent } from '@app/settings/settings-user/settings-user
 import { RPiComponentAddComponent } from '@app/settings/settings-rpicomponent/rpicomponent-add/rpicomponent-add.component';
 import { RPiComponentEditComponent } from '@app/settings/settings-rpicomponent/rpicomponent-edit/rpicomponent-edit.component';
 
+import { HumiditySensorFindAllResolve } from '@app/core/resolve/humidity-sensor-find-all.resolve';
+import { MoistureSensorFindAllResolve } from '@app/core/resolve/moisture-sensor-find-all.resolve';
+import { ProximitySensorFindAllResolve } from '@app/core/resolve/proximity-sensor-find-all.resolve';
+import { RelayFindAllResolve } from '@app/core/resolve/relay-find-all.resolve';
+import { TemperatureSensorFindAllResolve } from '@app/core/resolve/temperature-sensor-find-all.resolve';
+
 export const SETTINGS_ROUTES = [
   {
     path: '',
@@ -32,12 +38,12 @@ export const SETTINGS_ROUTES = [
         ]
       },
       {
-        path: 'component/:id/add',
+        path: 'component/:id',
         component: SettingsComponent,
         children: [
           {
             path: '',
-            component: RPiComponentAddComponent,
+            component: RPiComponentEditComponent,
             outlet: 'settings'
           }
         ]
