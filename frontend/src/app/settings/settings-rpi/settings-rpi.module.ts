15a43a46c7f191c9af47aa828f840f75216baa6e
Jason White <jmw5598@gmail.com>
2018-01-23 13:08:36 -0500

    updated settings-rpi.module.ts file with new components

--- frontend/src/app/settings/settings-rpi/settings-rpi.module.ts
+++ frontend/src/app/settings/settings-rpi/settings-rpi.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RpiComponent } from './rpi/rpi.component';
+import { RpiListComponent } from './rpi-list/rpi-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [RpiComponent, RpiListComponent]
 })
 export class SettingsRpiModule { }
