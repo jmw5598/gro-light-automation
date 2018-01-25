b37a4f0b886ee7f37c66054078ba91e3443900f2
Jason White <jmw5598@gmail.com>
2018-01-25 12:56:31 -0500

    added node state to panel heading, will need to hook in to system events when created

--- frontend/src/app/settings/settings-rpi/rpi/rpi.component.ts
+++ frontend/src/app/settings/settings-rpi/rpi/rpi.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { RPi } from '@app/shared/model/rpi/rpi.model';
+import { SystemState } from '@app/shared/model/rpi/system-state.enum';
 @Component({
   selector: 'gro-rpi',
   templateUrl: './rpi.component.html',
@@ -11,6 +12,8 @@ export class RPiComponent implements OnInit {
 
   @Input()
   rpi: RPi;
+
+  systemState = SystemState
 
   constructor(
     private router: Router
