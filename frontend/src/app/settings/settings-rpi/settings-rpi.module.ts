0604c2d0607f9cdab6ebac5cdfbe65ad2a9328f4
Jason White <jmw5598@gmail.com>
2018-01-25 09:30:23 -0500

    updated settings-rpi module importing new routing

--- frontend/src/app/settings/settings-rpi/settings-rpi.module.ts
+++ frontend/src/app/settings/settings-rpi/settings-rpi.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { RPiAddComponent } from './rpi-add/rpi-add.component';
 import { RPiComponent } from './rpi/rpi.component';
+import { RPiDetailsComponent } from './rpi-details/rpi-details.component';
+import { RPiFormComponent } from './rpi-form/rpi-form.component';
 import { RPiListComponent } from './rpi-list/rpi-list.component';
 import { SettingsRPiComponent } from './settings-rpi.component';
-import { RPiFormComponent } from './rpi-form/rpi-form.component';
-import { RPiAddComponent } from './rpi-add/rpi-add.component';
+
+import { SETTINGS_RPI_ROUTES } from './settings-rpi.routing';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(SETTINGS_RPI_ROUTES)
   ],
   exports: [
     SettingsRPiComponent
@@ -16,6 +23,7 @@ import { RPiAddComponent } from './rpi-add/rpi-add.component';
   declarations: [
     RPiAddComponent,
     RPiComponent,
+    RPiDetailsComponent,
     RPiFormComponent,
     RPiListComponent,
     SettingsRPiComponent
