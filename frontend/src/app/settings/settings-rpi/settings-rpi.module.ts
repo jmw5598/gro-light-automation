31e2450c534e93f880932fc6c6efb46f8c9f6adf
Jason White <jmw5598@gmail.com>
2018-01-27 15:28:27 -0500

    updated settings-rpi module imports

--- frontend/src/app/settings/settings-rpi/settings-rpi.module.ts
+++ frontend/src/app/settings/settings-rpi/settings-rpi.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '@app/shared/shared.module';
 import { RouterModule } from '@angular/router';
 
 import { RPiAddComponent } from './rpi-add/rpi-add.component';
 import { RPiComponent } from './rpi/rpi.component';
+import { RPiComponentTableComponent } from './rpicomponent-table/rpicomponent-table.component';
+import { RPiComponentFilterComponent } from './rpicomponent-table/rpicomponent-filter/rpicomponent-filter.component';
 import { RPiDetailsComponent } from './rpi-details/rpi-details.component';
 import { RPiFormComponent } from './rpi-form/rpi-form.component';
 import { RPiListComponent } from './rpi-list/rpi-list.component';
+import { RPiPinoutComponent } from './rpi-pinout/rpi-pinout.component';
 import { SettingsRPiComponent } from './settings-rpi.component';
-
+import { SettingsRPiComponentService } from './settings-rpicomponent.service';
 import { SETTINGS_RPI_ROUTES } from './settings-rpi.routing';
+import { RPiComponentFilterPipe } from './rpicomponent-table/rpicomponent-filter/rpicomponent-filter.pipe';
 
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     RouterModule.forChild(SETTINGS_RPI_ROUTES)
   ],
   exports: [
@@ -23,10 +27,17 @@ import { SETTINGS_RPI_ROUTES } from './settings-rpi.routing';
   declarations: [
     RPiAddComponent,
     RPiComponent,
+    RPiComponentTableComponent,
+    RPiComponentFilterComponent,
     RPiDetailsComponent,
     RPiFormComponent,
     RPiListComponent,
-    SettingsRPiComponent
+    RPiPinoutComponent,
+    SettingsRPiComponent,
+    RPiComponentFilterPipe
+  ],
+  providers: [
+    SettingsRPiComponentService
   ]
 })
 export class SettingsRPiModule { }
