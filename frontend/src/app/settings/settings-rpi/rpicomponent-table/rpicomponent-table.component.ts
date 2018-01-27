4f3013c057d856146c1aa34d6fa73d3c10bb13e6
Jason White <jmw5598@gmail.com>
2018-01-27 10:52:42 -0500

    updated rpicomponent-table to use rpi components as input

--- frontend/src/app/settings/settings-rpi/rpicomponent-table/rpicomponent-table.component.ts
+++ frontend/src/app/settings/settings-rpi/rpicomponent-table/rpicomponent-table.component.ts
@@ -16,26 +16,10 @@ import { ToastType } from '@app/core/component/toaster/toast-type.enum';
 })
 export class RPiComponentTableComponent implements OnInit, OnDestroy {
 
-  // Enum declaration for template
-  rPiComponentType = RPiComponentType
-
-  // Subscription data
-  humidity: Array<RPiComponent>;
-  moisture: Array<RPiComponent>;
-  proximity: Array<RPiComponent>;
-  relay: Array<RPiComponent>;
-  temperature: Array<RPiComponent>;
-  temperature_humidity: Array<RPiComponent>;
-
+  @Input()
+  components: Array<RPiComponent>;
+  
   filter: RPiComponentType;
-
-  // Subscriptions
-  humiditySubscription;
-  moistureSubscription;
-  proximitySubscription;
-  relaySubscription;
-  temperatureSubscription;
-  temperature_humiditySubscription;
 
   filterSubscription;
 
@@ -45,37 +29,6 @@ export class RPiComponentTableComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-
-    this.humiditySubscription = this.settingsRPiComponentService.humiditySensors
-      .subscribe(
-        data => this.humidity = data,
-        error => this.toasterService.toast("Error subscribing to humidity components", ToastType.WARNING)
-      );
-
-    this.moistureSubscription = this.settingsRPiComponentService.moistureSensors
-      .subscribe(
-        data => this.moisture = data,
-        error => this.toasterService.toast("Error subscribing to moisture components", ToastType.WARNING)
-      );
-
-    this.proximitySubscription = this.settingsRPiComponentService.proximitySensors
-      .subscribe(
-        data => this.proximity = data,
-        error => this.toasterService.toast("Error subscribing to proximity components", ToastType.WARNING)
-      );
-
-    this.relaySubscription = this.settingsRPiComponentService.relays
-      .subscribe(
-        data => this.relay = data,
-        error => this.toasterService.toast("Error subscribing to relay components", ToastType.WARNING)
-      );
-
-    this.temperatureSubscription = this.settingsRPiComponentService.temperatureSensors
-      .subscribe(
-        data => this.temperature = data,
-        error => this.toasterService.toast("Error subscribing to temperature components", ToastType.WARNING)
-      );
-
     this.filterSubscription = this.settingsRPiComponentService.componentFilter
       .subscribe(
         data => this.filter = data,
@@ -83,25 +36,11 @@ export class RPiComponentTableComponent implements OnInit, OnDestroy {
       );
   }
 
-  // switch(filter: RPiComponentType) {
-  //   this.settingsRPiComponentService.setComponentFilter(filter);
-  // }
-
   hover(component: RPiComponent) {
     console.log(component);
   }
 
   ngOnDestroy() {
-    if(this.humiditySubscription)
-      this.humiditySubscription.unsubscribe();
-    if(this.moistureSubscription)
-      this.moistureSubscription.unsubscribe();
-    if(this.proximitySubscription)
-      this.proximitySubscription.unsubscribe();
-    if(this.relaySubscription)
-      this.relaySubscription.unsubscribe();
-    if(this.temperatureSubscription)
-      this.temperatureSubscription.unsubscribe();
     if(this.filterSubscription)
       this.filterSubscription.unsubscribe();
   }
