ef198bd2da30267d60a15d23be4be492a71523e9
Jason White <jmw5598@gmail.com>
2018-02-02 13:13:22 -0500

    added endpoint to upload image for rpis

--- backend/src/main/java/com/gro/web/controller/RPiController.java
+++ backend/src/main/java/com/gro/web/controller/RPiController.java
@@ -4,9 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
