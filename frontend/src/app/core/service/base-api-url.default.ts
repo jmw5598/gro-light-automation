6253571f53b57b4e385342cf08ef8e1943e9093b
Jason White <jmw5598@gmail.com>
2018-02-02 14:06:50 -0500

    added default base resources url

--- frontend/src/app/core/service/base-api-url.default.ts
+++ frontend/src/app/core/service/base-api-url.default.ts
@@ -1,2 +1,3 @@
+export const BASE_IMAGE_URL: string = 'http://localhost:8080/images/';
 export const BASE_API_URL: string = 'http://localhost:8080/api/';
 export const BASE_AUTH_URL: string = 'http://localhost:8080/auth/';
